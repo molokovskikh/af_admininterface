8f7330e1681aee77e5d39604fd8c7caf1d7c38eb
Kvasov <Kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2006-04-18 10:29:00 +0000

    Код VB переведен на C#

--- src/AdminInterface/statcont.aspx
+++ src/AdminInterface/statcont.aspx
@@ -1,61 +1,69 @@
-<%@ Page Language="vb" AutoEventWireup="false" Inherits="AddUser.statcont" codePage="1251" CodeFile="statcont.aspx.vb" %>
-<HTML>
-	<HEAD>
-		<title>Статистика обращений</title>
-		<meta content="False" name="vs_showGrid">
-		<meta content="http://schemas.microsoft.com/intellisense/ie5" name="vs_targetSchema">
-		<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
-	</HEAD>
-	<body vLink="#ab51cc" aLink="#0093e1" link="#0093e1" bgColor="#ffffff">
-		<form id="Form1" method="post" runat="server">
-			<DIV align="center">
-				<TABLE id="Table2" cellSpacing="0" cellPadding="0" width="320" align="center" border="0">
-					<TR>
-						<TD colSpan="2">
-							<P align="center"><FONT face="Arial" size="2"><STRONG>Выберите&nbsp;период</STRONG></FONT></P>
-						</TD>
-					</TR>
-					<TR>
-						<TD><FONT face="Arial" size="2"><STRONG>C:</STRONG></FONT></TD>
-						<TD colSpan="1"><FONT face="Arial" size="2"><STRONG>По:</STRONG></FONT></TD>
-					</TR>
-					<TR>
-						<TD>
-							<P align="right">
-								<asp:calendar id="CalendarFrom" runat="server" Font-Names="Arial" Font-Size="10pt" TitleFormat="Month"
-									ShowGridLines="True" BackColor="#F6F6F6">
-									<SelectedDayStyle ForeColor="Black" BackColor="#0093E1"></SelectedDayStyle>
-								</asp:calendar></P>
-						</TD>
-						<TD>
-							<P align="right">
-								<asp:calendar id="CalendarTo" runat="server" Font-Names="Arial" Font-Size="10pt" TitleFormat="Month"
-									ShowGridLines="True" BackColor="#F6F6F6" FirstDayOfWeek="Monday">
-									<SelectedDayStyle ForeColor="Black" BackColor="#0093E1"></SelectedDayStyle>
-								</asp:calendar></P>
-						</TD>
-					</TR>
-					<TR>
-						<TD colSpan="2">
-							<P align="right">
-								<asp:button id="Button1" runat="server" Font-Names="Arial" Font-Size="10pt" Text="Показать"
-									BorderStyle="None"></asp:button></P>
-						</TD>
-					</TR>
-				</TABLE>
-			</DIV>
-	
-			<P align="center"><asp:table id="Table5" runat="server" Font-Names="Arial" Font-Size="8pt" BackColor="#EBEBEB"
-					GridLines="Both" CellPadding="0" CellSpacing="0" BorderColor="#DADADA" BorderStyle="Solid">
-					<asp:TableRow VerticalAlign="Middle" HorizontalAlign="Center" BackColor="AliceBlue" Font-Size="10pt"
-						Font-Names="Arial" Font-Bold="True">
-						<asp:TableCell Width="90px" Text="Дата"></asp:TableCell>
-						<asp:TableCell Width="90px" Text="Оператор"></asp:TableCell>
-						<asp:TableCell Width="120px" Text="Клиент"></asp:TableCell>
-						<asp:TableCell Width="100px" Text="Регион"></asp:TableCell>
-						<asp:TableCell Text="Сообщение"></asp:TableCell>
-					</asp:TableRow>
-				</asp:table></P>
-		</form>
-	</body>
-</HTML>
+<%@ Page Language="c#" AutoEventWireup="true" Inherits="AddUser.statcont" CodePage="1251"
+    CodeFile="statcont.aspx.cs" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Статистика обращений</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+</head>
+<body vlink="#ab51cc" alink="#0093e1" link="#0093e1" bgcolor="#ffffff">
+    <form id="Form1" method="post" runat="server">
+        <div align="center">
+            <table id="Table2" cellspacing="0" cellpadding="0" width="320" align="center" border="0">
+                <tr>
+                    <td colspan="2">
+                        <p align="center">
+                            <font face="Arial" size="2"><strong>Выберите&nbsp;период</strong></font></p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <font face="Arial" size="2"><strong>C:</strong></font></td>
+                    <td colspan="1">
+                        <font face="Arial" size="2"><strong>По:</strong></font></td>
+                </tr>
+                <tr>
+                    <td>
+                        <p align="right">
+                            <asp:Calendar ID="CalendarFrom" runat="server" OnSelectionChanged="CalendarFrom_SelectionChanged" Font-Names="Arial" Font-Size="10pt"
+                                TitleFormat="Month" ShowGridLines="True" BackColor="#F6F6F6">
+                                <SelectedDayStyle ForeColor="Black" BackColor="#0093E1"></SelectedDayStyle>
+                            </asp:Calendar>
+                        </p>
+                    </td>
+                    <td>
+                        <p align="right">
+                            <asp:Calendar ID="CalendarTo" runat="server" OnSelectionChanged="CalendarTo_SelectionChanged" Font-Names="Arial" Font-Size="10pt"
+                                TitleFormat="Month" ShowGridLines="True" BackColor="#F6F6F6" FirstDayOfWeek="Monday">
+                                <SelectedDayStyle ForeColor="Black" BackColor="#0093E1"></SelectedDayStyle>
+                            </asp:Calendar>
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <p align="right">
+                            <asp:Button ID="Button1" runat="server" Font-Names="Arial" Font-Size="10pt" Text="Показать"
+                                BorderStyle="None"></asp:Button></p>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <p align="center">
+            <asp:Table ID="Table5" runat="server" Font-Names="Arial" Font-Size="8pt" BackColor="#EBEBEB"
+                GridLines="Both" CellPadding="0" CellSpacing="0" BorderColor="#DADADA" BorderStyle="Solid">
+                <asp:TableRow VerticalAlign="Middle" HorizontalAlign="Center" BackColor="AliceBlue"
+                    Font-Size="10pt" Font-Names="Arial" Font-Bold="True">
+                    <asp:TableCell Width="90px" Text="Дата"></asp:TableCell>
+                    <asp:TableCell Width="90px" Text="Оператор"></asp:TableCell>
+                    <asp:TableCell Width="120px" Text="Клиент"></asp:TableCell>
+                    <asp:TableCell Width="100px" Text="Регион"></asp:TableCell>
+                    <asp:TableCell Text="Сообщение"></asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+        </p>
+    </form>
+</body>
+</html>
--- src/AdminInterface/statcont.aspx.cs
+++ src/AdminInterface/statcont.aspx.cs
@@ -1,215 +1,216 @@
-'mports ByteFX.Data.MySqlClient
-Imports MySql.Data.MySqlClient
+using System;
+using System.Drawing;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using Image = System.Web.UI.WebControls.Image;
+
+namespace AddUser
+{
+	public partial class statcont : Page
+	{
+		public int ob;
+		public string Picstr;
+		public HyperLink HL = new HyperLink();
 
 
-Namespace AddUser
+		private void ShowStatS()
+		{
+			Image Pic;
+			MySqlCommand Комманда = new MySqlCommand();
+			MySqlDataReader Reader;
+			TableRow row;
+			TableCell cell;
+			string Order = string.Empty;
+			MySqlConnection соединение = new MySqlConnection(Literals.GetConnectionString());
+			HL = new HyperLink();
+			Pic = new Image();
+			Pic.ImageUrl = Picstr;
+			if (ob == 0)
+			{
+				Table5.Rows[0].Cells[0].BackColor = ColorTranslator.FromHtml("#D8F1FF");
+				Table5.Rows[0].Cells[0].Controls.Add(Pic);
+				Order = Order + "writetime";
+			}
+			if (ob == 1)
+			{
+				Table5.Rows[0].Cells[1].BackColor = ColorTranslator.FromHtml("#D8F1FF");
+				Table5.Rows[0].Cells[1].Controls.Add(Pic);
+				Order = Order + "clientsinfo.UserName";
+			}
+			if (ob == 2)
+			{
+				Table5.Rows[0].Cells[2].BackColor = ColorTranslator.FromHtml("#D8F1FF");
+				Table5.Rows[0].Cells[2].Controls.Add(Pic);
+				Order = Order + "ShortName";
+			}
+			if (ob == 3)
+			{
+				Table5.Rows[0].Cells[3].BackColor = ColorTranslator.FromHtml("#D8F1FF");
+				Table5.Rows[0].Cells[3].Controls.Add(Pic);
+				Order = Order + "Region";
+			}
+			if (Picstr == "arrow-down-blue-reversed.gif")
+				Order = Order + " desc";
+			соединение.Open();
+			
+			Комманда.CommandText =
+				" SELECT WriteTime, clientsinfo.UserName, ShortName, Region, Message, FirmCode, osuseraccessright.rowid, clientsinfo.rowid" +
+				" FROM logs.clientsinfo, usersettings.clientsdata, accessright.showright, usersettings.osuseraccessright, farm.regions" +
+				" where clientsinfo.clientcode=firmcode  and osuseraccessright.clientcode=clientsinfo.clientcode" +
+				" and showright.RegionMask & clientsdata.RegionCode > 0 and writetime between ?FromDate and ?ToDate" +
+				" and regions.regioncode=clientsdata.RegionCode and showright.username=?userName order by ?order";
+			Комманда.Parameters.Add("?userName", Session["UserName"]);
+			Комманда.Parameters.Add("?order", Order);
+			
+			Комманда.Connection = соединение;
+			Комманда.Parameters.Add(new MySqlParameter("FromDate", MySqlDbType.Datetime));
+			Комманда.Parameters["FromDate"].Value = CalendarFrom.SelectedDate;
+			Комманда.Parameters.Add(new MySqlParameter("ToDate", MySqlDbType.Datetime));
+			Комманда.Parameters["ToDate"].Value = CalendarTo.SelectedDate.AddDays(1);
+			Reader = Комманда.ExecuteReader();
+			if (Request.Cookies["Inforoom.Admins.ShowStatsC"] == null)
+			{
+				Response.Cookies["Inforoom.Admins.ShowStatsC"].Value = "0";
+				Response.Cookies["Inforoom.Admins.ShowStatsC"].Expires = DateTime.Now.AddYears(2);
+			}
+			int OMaxID = Convert.ToInt32(Request.Cookies["Inforoom.Admins.ShowStatsC"].Value);
+			int MaxID = 0;
+			while (Reader.Read())
+			{
+				if (Convert.ToInt32(Reader[7]) > MaxID)
+				{
+					MaxID = Convert.ToInt32(Reader[7]);
+				}
+				row = new TableRow();
+				if (OMaxID < Convert.ToInt32(Reader[7]))
+				{
+					row.BackColor = Color.White;
+				}
+				for (int i = 0; i <= Table5.Rows[0].Cells.Count - 1; i++)
+				{
+					cell = new TableCell();
+					if (ob == i)
+					{
+						cell.BackColor = Color.AliceBlue;
+					}
+					if (i == 2)
+					{
+						HL = new HyperLink();
+						HL.Text = Reader[i].ToString();
+						HL.NavigateUrl = "info.aspx?cc=" + Reader[5].ToString() + "&ouar=" + Reader[6].ToString();
+						cell.Controls.Add(HL);
+						HL.Dispose();
+					}
+					else if (i == 0)
+					{
+						DateTime PriceDate = Convert.ToDateTime(Reader[i]);
+						cell.Text = PriceDate.ToString("dd.MM.yy HH:mm");
+						cell.Font.Size = FontUnit.Point(8);
+					}
+					else
+					{
+						cell.Text = Reader[i].ToString();
+					}
+					row.Cells.Add(cell);
+				}
+				Table5.Rows.Add(row);
+			}
+			Reader.Close();
+			Session["MaxID"] = MaxID;
+			соединение.Close();
+		}
 
-Partial Class statcont
-    Inherits System.Web.UI.Page
-    Dim ob As Int16
-    Dim Picstr As String
-    Dim HL As New HyperLink()
-#Region " Web Form Designer Generated Code "
 
-    'This call is required by the Web Form Designer.
-    <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
-
-    End Sub
-
-    Private Sub Page_Init(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Init
-        'CODEGEN: This method call is required by the Web Form Designer
-        'Do not modify it using the code editor.
-        InitializeComponent()
-    End Sub
-
-#End Region
-
-        'Private Sub Page_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-        'End Sub
-
-    Private Sub ShowStatS()
-
-        Dim Pic As System.Web.UI.WebControls.Image
-        Dim Комманда As New MySqlCommand()
-        Dim Reader As MySqlDataReader
-        Dim row As New TableRow()
-        Dim cell As New TableCell()
-        Dim i, MaxID, OMaxID As Int16
-        Dim Order As String
-        Dim соединение As New MySqlConnection("Data Source=testsql.analit.net;Database=usersettings;User ID=system;Password=123;Connect Timeout=300;")
+		protected void CalendarTo_SelectionChanged(object sender, EventArgs e)
+		{
+			Session["SelectedToDate"] = CalendarTo.SelectedDate;
+			ShowStatS();
+		}
 
 
+		protected void CalendarFrom_SelectionChanged(object sender, EventArgs e)
+		{
+			Session["SelectedFromDate"] = CalendarFrom.SelectedDate;
+			ShowStatS();
+		}
 
-        HL = New HyperLink()
 
-        Pic = New System.Web.UI.WebControls.Image()
-        Pic.ImageUrl = Picstr
-
-        If ob = 0 Then
-            Table5.Rows(0).Cells(0).BackColor = Drawing.ColorTranslator.FromHtml("#D8F1FF")
-            Table5.Rows(0).Cells(0).Controls.Add(Pic)
-            Order = Order & "writetime"
-        End If
-        If ob = 1 Then
-            Table5.Rows(0).Cells(1).BackColor = Drawing.ColorTranslator.FromHtml("#D8F1FF")
-            Table5.Rows(0).Cells(1).Controls.Add(Pic)
-            Order = Order & "clientsinfo.UserName"
-        End If
-        If ob = 2 Then
-            Table5.Rows(0).Cells(2).BackColor = Drawing.ColorTranslator.FromHtml("#D8F1FF")
-            Table5.Rows(0).Cells(2).Controls.Add(Pic)
-            Order = Order & "ShortName"
-        End If
-        If ob = 3 Then
-            Table5.Rows(0).Cells(3).BackColor = Drawing.ColorTranslator.FromHtml("#D8F1FF")
-            Table5.Rows(0).Cells(3).Controls.Add(Pic)
-            Order = Order & "Region"
-        End If
-        If Picstr = "arrow-down-blue-reversed.gif" Then Order = Order & " desc"
-
-        соединение.Open()
-        With Комманда
-            .CommandText = " SELECT WriteTime, clientsinfo.UserName, ShortName, Region, Message, FirmCode, osuseraccessright.rowid, clientsinfo.rowid" & _
-            " FROM logs.clientsinfo, usersettings.clientsdata, accessright.showright, osuseraccessright, farm.regions" & _
-            " where clientsinfo.clientcode=firmcode" & _
-            " and osuseraccessright.clientcode=clientsinfo.clientcode" & _
-            " and showright.RegionMask & clientsdata.RegionCode>0" & _
-            " and writetime between ?FromDate and ?ToDate" & _
-              " and regions.regioncode=clientsdata.RegionCode" & _
-            " and showright.username='" & Session("UserName") & "' order by " & Order
-            .Connection = соединение
-            .Parameters.Add(New MySqlParameter("FromDate", MySqlDbType.Datetime))
-            .Parameters("FromDate").Value = CalendarFrom.SelectedDate
-
-            .Parameters.Add(New MySqlParameter("ToDate", MySqlDbType.Datetime))
-            .Parameters("ToDate").Value = CalendarTo.SelectedDate.AddDays(1)
-
-            '.Parameters.Add(New MySqlParameter("@Login", MySqlDbType.String))
-            '.Parameters("@Login").Value = "%" & LoginTB.Text & "%"
-
-            Reader = .ExecuteReader
-        End With
-        If Request.Cookies("Inforoom.Admins.ShowStatsC") Is Nothing Then
-            Response.Cookies("Inforoom.Admins.ShowStatsC").Value = 0
-            Response.Cookies("Inforoom.Admins.ShowStatsC").Expires = Now().AddYears(2)
-        End If
-        OMaxID = Request.Cookies("Inforoom.Admins.ShowStatsC").Value
-        MaxID = 0
-        While Reader.Read
-            If Reader.Item(7).ToString > MaxID Then MaxID = Reader.Item(7).ToString
-            row = New TableRow()
-            If OMaxID < Reader.Item(7) Then row.BackColor = Color.White
-            For i = 0 To Table5.Rows(0).Cells.Count - 1
-                cell = New TableCell()
-                If ob = i Then cell.BackColor = Drawing.Color.AliceBlue
-                If i = 2 Then
-                    HL = New HyperLink()
-                    HL.Text = Reader.Item(i)
-                    HL.NavigateUrl = "info.aspx?cc=" & Reader.Item(5).ToString & "&ouar=" & Reader.Item(6).ToString
-                    cell.Controls.Add(HL)
-                    HL.Dispose()
-                ElseIf i = 0 Then
-                    Dim PriceDate As Date = Reader.Item(i)
-                    Dim Min, Month, Day As String
-                    Min = PriceDate.Minute
-                    If Len(Min) < 2 Then Min = 0 & Min
-                    Month = PriceDate.Month
-                    If Len(Month) < 2 Then Month = 0 & Month
-                    Day = PriceDate.Day
-                    If Len(Day) < 2 Then Day = 0 & Day
-                    cell.Text = Day & "." & Month & "." & Right(PriceDate.Year, 2) & " " & PriceDate.Hour & ":" & Min
-                    cell.Font.Size = WebControls.FontUnit.Point(8)
-                Else
-                    cell.Text = Reader.Item(i).ToString
-                End If
-
-                row.Cells.Add(cell)
-            Next
-            Table5.Rows.Add(row)
-        End While
-        Reader.Close()
-        Session("MaxID") = MaxID
-        соединение.Close()
-
-    End Sub
-
-    Private Sub CalendarTo_SelectionChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles CalendarTo.SelectionChanged
-        Session("SelectedToDate") = CalendarTo.SelectedDate
-        ShowStatS()
-    End Sub
-
-    Private Sub CalendarFrom_SelectionChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles CalendarFrom.SelectionChanged
-        Session("SelectedFromDate") = CalendarFrom.SelectedDate
-        ShowStatS()
-    End Sub
-
-        Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-            If Session.Item("AccessGrant") <> 1 Then Response.Redirect("default.aspx")
-
-            If Not IsPostBack Then
-
-                If Not Session("SelectedFromDate") Is Nothing Then
-                    CalendarFrom.SelectedDate = Session("SelectedFromDate")
-                Else
-                    CalendarFrom.SelectedDate = Now().AddDays(-14)
-                End If
-
-                If Not Session("SelectedToDate") Is Nothing Then
-                    CalendarTo.SelectedDate = Session("SelectedToDate")
-                Else
-                    CalendarTo.SelectedDate = Now()
-                End If
-
-                If Len(Request("ob")) < 1 Then
-                    If Not Request.Cookies("Inforoom.Stat.OrderBy") Is Nothing Then
-                        ob = CInt(Request.Cookies("Inforoom.Stat.OrderBy").Value)
-                    Else
-                        ob = 0
-                    End If
-                Else
-                    If CInt(Session("ob")) = CInt(Request("ob")) Then
-                        If Session("Picstr") = "arrow-down-blue.gif" Then Picstr = "arrow-down-blue-reversed.gif"
-                    End If
-                    Session("Picstr") = Picstr
-                    ob = CInt(Request("ob"))
-                    Response.Cookies("Inforoom.Stat.OrderBy").Value = ob
-                    Response.Cookies("Inforoom.Stat.OrderBy").Expires = Now().AddYears(2)
-                End If
-
-                If Len(Session("Picstr")) < 1 Then
-                    Picstr = "arrow-down-blue.gif"
-                    Session("Picstr") = Picstr
-                End If
-                Session("ob") = ob
-                ShowStatS()
-            End If
-            HL.NavigateUrl = "statcont.aspx?Ob=0"
-            HL.Text = "Дата"
-            'HL.ForeColor = Color.Black
-            Table5.Rows(0).Cells(0).Controls.Add(HL)
-
-            HL = New HyperLink
-            HL.Text = "Оператор"
-            'HL.ForeColor = Color.Black
-            HL.NavigateUrl = "statcont.aspx?Ob=1"
-            Table5.Rows(0).Cells(1).Controls.Add(HL)
-
-            HL = New HyperLink
-            HL.Text = "Клиент"
-            'HL.ForeColor = Color.Black
-            HL.NavigateUrl = "statcont.aspx?Ob=2"
-            Table5.Rows(0).Cells(2).Controls.Add(HL)
-
-            HL = New HyperLink
-            HL.Text = "Регион"
-            'HL.ForeColor = Color.Black
-            HL.NavigateUrl = "statcont.aspx?Ob=3"
-            Table5.Rows(0).Cells(3).Controls.Add(HL)
-
-            Picstr = Session("Picstr")
-            ob = Session("ob")
-        End Sub
-    End Class
-
-End Namespace
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (Convert.ToInt32(Session["AccessGrant"]) != 1)
+			{
+				Response.Redirect("default.aspx");
+			}
+			if (!(IsPostBack))
+			{
+				if (Session["SelectedFromDate"] != null)
+				{
+					CalendarFrom.SelectedDate = Convert.ToDateTime(Session["SelectedFromDate"]);
+				}
+				else
+				{
+					CalendarFrom.SelectedDate = DateTime.Now.AddDays(-14);
+				}
+				if (Session["SelectedToDate"] != null)
+				{
+					CalendarTo.SelectedDate = Convert.ToDateTime(Session["SelectedToDate"]);
+				}
+				else
+				{
+					CalendarTo.SelectedDate = DateTime.Now;
+				}
+				if (String.IsNullOrEmpty(Request["ob"]))
+				{
+					if (!(Request.Cookies["Inforoom.Stat.OrderBy"] == null))
+					{
+						ob = Convert.ToInt32(Request.Cookies["Inforoom.Stat.OrderBy"].Value);
+					}
+					else
+					{
+						ob = 0;
+					}
+				}
+				else
+				{
+					if (Convert.ToInt32(Session["ob"]) == Convert.ToInt32(Request["ob"]))
+					{
+						if (Session["Picstr"] == "arrow-down-blue.gif")
+						{
+							Picstr = "arrow-down-blue-reversed.gif";
+						}
+					}
+					Session["Picstr"] = Picstr;
+					ob = Convert.ToInt32(Request["ob"]);
+					Response.Cookies["Inforoom.Stat.OrderBy"].Value = ob.ToString();
+					Response.Cookies["Inforoom.Stat.OrderBy"].Expires = DateTime.Now.AddYears(2);
+				}
+				
+				if (String.IsNullOrEmpty((string)Session["Picstr"]))
+				{
+					Picstr = "arrow-down-blue.gif";
+					Session["Picstr"] = Picstr;
+				}
+				Session["ob"] = ob;
+				ShowStatS();
+			}
+			HL.NavigateUrl = "statcont.aspx?Ob=0";
+			HL.Text = "Дата";
+			Table5.Rows[0].Cells[0].Controls.Add(HL);
+			HL = new HyperLink();
+			HL.Text = "Оператор";
+			HL.NavigateUrl = "statcont.aspx?Ob=1";
+			Table5.Rows[0].Cells[1].Controls.Add(HL);
+			HL = new HyperLink();
+			HL.Text = "Клиент";
+			HL.NavigateUrl = "statcont.aspx?Ob=2";
+			Table5.Rows[0].Cells[2].Controls.Add(HL);
+			HL = new HyperLink();
+			HL.Text = "Регион";
+			HL.NavigateUrl = "statcont.aspx?Ob=3";
+			Table5.Rows[0].Cells[3].Controls.Add(HL);
+			Picstr = Session["Picstr"].ToString();
+			ob = Convert.ToInt32(Session["ob"]);
+		}
+	}
+}
