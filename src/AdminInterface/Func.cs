17bdd178c73e2e113fd21b3d5052efc6bc042ba2
Kvasov <Kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2006-04-21 07:34:00 +0000

    Код VB переведен на C#

--- src/AdminInterface/Func.cs
+++ src/AdminInterface/Func.cs
@@ -1,148 +1,240 @@
-Imports System.Web.Mail
-'Imports ByteFX.Data.MySqlClient
-Imports MySql.Data.MySqlClient
+using System;
+using System.Configuration;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Web.Mail;
+using MySql.Data.MySqlClient;
+
+namespace AddUser
+{
+	public class Func
+	{
+		public static void Mail(string From, string Subject, MailFormat BodyFormat, string Body, string MessageTo,
+		                        string MessageBCC, Encoding Encoding)
+		{
+			try
+			{
+				MailMessage message = new MailMessage();
+				message.From = From;
+				message.Subject = Subject;
+				message.BodyFormat = BodyFormat;
+				message.Body = Body;
+				message.Bcc = MessageBCC;
+				message.To = MessageTo;
+				message.BodyEncoding = Encoding;
+				SmtpMail.SmtpServer = "box.analit.net";
+				SmtpMail.Send(message);
+			}
+			catch
+			{
+			}
+		}
+
+		public static string GeneratePassword()
+		{
+			Random random = new Random();
+			Int16 r;
+			string PassStr = String.Empty;
+			try
+			{
+				for (int i = 0; i <= 7; i++)
+				{
+					r = Convert.ToInt16((62*Convert.ToSingle(random.NextDouble())) + 1);
+					if ((r >= 0) && (r < 26))
+					{
+						if ((65 + r) == 73)
+							r = Convert.ToInt16(r + 1);
+						PassStr = PassStr + (char) (65 + r);
+					}
+					if ((r >= 26) && (r < 52))
+					{
+						if ((71 + Convert.ToInt32(r)) == 108)
+							r = Convert.ToInt16(r + 1);
+						PassStr = PassStr + (char) (71 + r);
+					}
+					if (r >= 52)
+					{
+						PassStr = PassStr + (char) (48 + Convert.ToInt32(Convert.ToSingle(random.NextDouble())*9));
+					}
+				}
+				if (PassStr.Length < 8)
+				{
+					r = Convert.ToInt16(97 + (DateTime.Now.Second/3));
+					if (Convert.ToInt32(r) == 108)
+					{
+						r = Convert.ToInt16(r + 1);
+					}
+					PassStr = PassStr + (char) r;
+				}
+				return PassStr;
+			}
+			catch
+			{
+				return "[Ошибка формирования пароля]";
+			}
+		}
+
+		public static bool SelectTODS(string SQLQuery, string Table, DataSet DS, MySqlCommand MySQLCommand, string CommandAdd)
+		{
+			MySqlCommand myMySqlCommand = new MySqlCommand();
+			MySqlConnection myMySqlConnection = new MySqlConnection(ConfigurationManager.AppSettings["ConnectionString"]);
+			MySqlCommand Комманда = new MySqlCommand();
+			MySqlDataAdapter myMySqlDataAdapter = new MySqlDataAdapter();
+			try
+			{
+				myMySqlConnection.Open();
+			}
+			catch
+			{
+				return false;
+			}
+			try
+			{
+				if (!(MySQLCommand == null))
+				{
+					myMySqlCommand = MySQLCommand;
+				}
+				myMySqlCommand.CommandText = SQLQuery + CommandAdd;
+				myMySqlCommand.Connection = myMySqlConnection;
+				myMySqlDataAdapter.SelectCommand = myMySqlCommand;
+				myMySqlDataAdapter.Fill(DS, Table);
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+			finally
+			{
+				Комманда.Dispose();
+				myMySqlDataAdapter.Dispose();
+				myMySqlConnection.Close();
+				myMySqlConnection.Dispose();
+			}
+		}
 
 
-Namespace AddUser
+		public static bool SelectTODS(string SQLQuery, string Table, DataSet DS, MySqlCommand MySQLCommand)
+		{
+			string CommandAdd = String.Empty;
+			MySqlCommand myMySqlCommand = new MySqlCommand();
+			MySqlConnection myMySqlConnection = new MySqlConnection(ConfigurationManager.AppSettings["ConnectionString"]);
+			MySqlCommand Комманда = new MySqlCommand();
+			MySqlDataAdapter myMySqlDataAdapter = new MySqlDataAdapter();
+			try
+			{
+				myMySqlConnection.Open();
+			}
+			catch
+			{
+				return false;
+			}
+			try
+			{
+				if (!(MySQLCommand == null))
+				{
+					myMySqlCommand = MySQLCommand;
+				}
+				myMySqlCommand.CommandText = SQLQuery + CommandAdd;
+				myMySqlCommand.Connection = myMySqlConnection;
+				myMySqlDataAdapter.SelectCommand = myMySqlCommand;
+				myMySqlDataAdapter.Fill(DS, Table);
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+			finally
+			{
+				Комманда.Dispose();
+				myMySqlDataAdapter.Dispose();
+				myMySqlConnection.Close();
+				myMySqlConnection.Dispose();
+			}
+		}
 
-Public Class Func
-        Public Sub Mail(ByVal From As String, ByVal Subject As String, ByVal BodyFormat As MailFormat, ByVal Body As String, ByVal MessageTo As String, ByVal MessageBCC As String, ByVal Encoding As System.Text.Encoding)
+		public static bool SelectTODS(string SQLQuery, string Table, DataSet DS)
+		{
+			MySqlCommand MySQLCommand = null;
+			string CommandAdd = String.Empty;
+			MySqlCommand myMySqlCommand = new MySqlCommand();
+			MySqlConnection myMySqlConnection = new MySqlConnection(ConfigurationManager.AppSettings["ConnectionString"]);
+			MySqlCommand Комманда = new MySqlCommand();
+			MySqlDataAdapter myMySqlDataAdapter = new MySqlDataAdapter();
+			try
+			{
+				myMySqlConnection.Open();
+			}
+			catch
+			{
+				return false;
+			}
+			try
+			{
+				if (!(MySQLCommand == null))
+				{
+					myMySqlCommand = MySQLCommand;
+				}
+				myMySqlCommand.CommandText = SQLQuery + CommandAdd;
+				myMySqlCommand.Connection = myMySqlConnection;
+				myMySqlDataAdapter.SelectCommand = myMySqlCommand;
+				myMySqlDataAdapter.Fill(DS, Table);
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+			finally
+			{
+				Комманда.Dispose();
+				myMySqlDataAdapter.Dispose();
+				myMySqlConnection.Close();
+				myMySqlConnection.Dispose();
+			}
+		}
 
-            Try
-                Dim message As New MailMessage
-                message.From = From
-                message.Subject = Subject
-                message.BodyFormat = BodyFormat
-                message.Body = Body
-                message.Bcc = MessageBCC
-                'message.Cc = MessageCC
-                message.To = MessageTo
-                message.BodyEncoding = Encoding
-                SmtpMail.SmtpServer = "box.analit.net"
-                SmtpMail.Send(message)
+		public static decimal GetDecimal(string InputStr)
+		{
+			if (InputStr.Length < 1)
+				return 0;
+			else
+			{
+				try
+				{
+					return Convert.ToDecimal(InputStr);
+				}
+				catch
+				{
+					new Exception(
+						String.Format(
+							"Не верно указанна скидка {0}. Для указания десятичных долей используйте знаки \".\" или \",\", к примеру 5.54",
+							InputStr));
+				}
+			}
+			return 0;
+		}
 
-            Catch
-            End Try
-        End Sub
 
-    Public Function GeneratePassword() As String
-        Randomize()
-        Dim i, r As Int16
-        Dim PassStr As String
-        Try
-            For i = 0 To 7 'должно быть 7
-                r = CInt(Int((62 * Rnd()) + 1))
-                If CInt(r) >= 0 And CInt(r) < 26 Then
-                    If (65 + CInt(r)) = 73 Then r = CInt(r) + 1
-                    PassStr = PassStr + Chr(65 + CInt(r))
-                End If
-                If CInt(r) >= 26 And CInt(r) < 52 Then
-                    If (71 + CInt(r)) = 108 Then r = CInt(r) + 1
-                    PassStr = PassStr + Chr(71 + CInt(r))
-                End If
-                If CInt(r) >= 52 Then PassStr = PassStr + Chr(48 + CInt(Rnd() * 9))
-            Next
-            If Len(PassStr) < 8 Then
-                r = 97 + CInt(Now.Second / 3)
-                If (CInt(r) = 108) Then
-                    r = CInt(r) + 1
-                End If
-                PassStr = PassStr + Chr(CInt(r))
-            End If
-            GeneratePassword = PassStr
-        Catch
-            GeneratePassword = "[Ошибка формирования пароля]"
-        End Try
-    End Function
+		public static string GetFirmType(int FrimTypeCode)
+		{
+			string FirmTypeStr;
+			if (FrimTypeCode == 0)
+				FirmTypeStr = "Поставщик";
+			else
+				FirmTypeStr = "Аптека";
+			return FirmTypeStr;
+		}
 
-    Public Function SelectTODS(ByVal SQLQuery As String, ByVal Table As String, ByVal DS As DataSet, Optional ByVal MySQLCommand As MySqlCommand = Nothing, Optional ByVal CommandAdd As String = "") As Boolean
-        Dim myMySqlCommand As New MySqlCommand
-        Dim myMySqlConnection As New MySqlConnection("Data Source=testsql.analit.net;Database=usersettings;User ID=system;Password=123;Connect Timeout=300;")
-        Dim Комманда As New MySqlCommand
-        Dim myMySqlDataAdapter As New MySqlDataAdapter
-        Try
-            myMySqlConnection.Open()
-        Catch
-            Exit Function
-        End Try
-        Try
-            If Not MySQLCommand Is Nothing Then myMySqlCommand = MySQLCommand
-            With myMySqlCommand
-                .CommandText = SQLQuery & CommandAdd
-                .Connection = myMySqlConnection
-            End With
-            myMySqlDataAdapter.SelectCommand = myMySqlCommand
-            myMySqlDataAdapter.Fill(DS, Table)
-            SelectTODS = True
-        Catch err As Exception
-            SelectTODS = False
-        Finally
-            Комманда.Dispose()
-            myMySqlDataAdapter.Dispose()
-            myMySqlConnection.Close()
-            myMySqlConnection.Dispose()
-        End Try
-    End Function
-
-    Public Function GetDecimal(ByVal InputStr As String) As Decimal
-        Dim str() As String
-        Dim Delimiter, ResStr As String
-        Dim i, q, w As Int16
-        If InputStr.Length < 1 Then
-            ResStr = 0
-        Else
-
-            For i = 1 To InputStr.Length
-                If Asc(Mid(InputStr, i, 1)) > 57 Then Err.Raise(1, "Не верно указанна скидка(" & InputStr & ").", "Для указания десятичных долей используйте знаки ""."" или "","", к примеру 5.54")
-            Next
-
-            For i = 1 To InputStr.Length
-                If Asc(Mid(InputStr, i, 1)) = 44 Or Asc(Mid(InputStr, i, 1)) = 46 Then Delimiter = Mid(InputStr, i, 1)
-            Next
-
-            str = Split(InputStr, Delimiter, 2, )
-            If str(0).Length < 1 Then str(0) = 0
-
-            For i = 0 To str.Length - 1
-                If i = 0 Then
-                    If Asc(Mid(str(i), 1)) = 45 Then
-                        w = 2
-                    Else
-                        w = 1
-                    End If
-                End If
-                For q = w To str(i).Length
-                    If (Asc(Mid(str(i), q, 1)) > 57 Or Asc(Mid(str(i), q, 1)) < 48) And Asc(Mid(str(i), q, 1)) <> 45 Then Err.Raise(1, "Не верно указанна скидка(" & InputStr & ").", "Для указания десятичных долей используйте знаки ""."" или "",""")
-                Next
-            Next
-
-            ResStr = str(0)
-            If str.Length > 1 Then
-                For q = 1 To str(1).Length
-                    If Asc(Mid(str(1), q, 1)) > 57 Or Asc(Mid(str(1), q, 1)) < 48 Then Err.Raise(1, "Не верно указанна скидка(" & InputStr & ").", "Для указания десятичных долей используйте знаки ""."" или "",""")
-                Next
-                If str(1).Length < 1 Then str(1) = 0
-                ResStr = ResStr & "," & str(1)
-            End If
-        End If
-        GetDecimal = CDec(ResStr)
-    End Function
-
-    Public Function GetFirmType(ByVal FrimTypeCode As Integer) As String
-        Dim FirmTypeStr As String
-        If FrimTypeCode = 0 Then
-            FirmTypeStr = "Поставщик"
-        Else
-            FirmTypeStr = "Аптека"
-        End If
-        GetFirmType = FirmTypeStr
-    End Function
-
-        Public Function CalcDays(ByVal CurContDate As Date) As Boolean
-            If Now().Subtract(CurContDate).TotalDays > 3 Then Return False
-            Return True
-        End Function
-
-End Class
-
-End Namespace
+		public bool CalcDays(DateTime CurContDate)
+		{
+			if (DateTime.Now.Subtract(CurContDate).TotalDays > 3)
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+}
--- src/AdminInterface/Properties/AssemblyInfo.cs
+++ src/AdminInterface/Properties/AssemblyInfo.cs
@@ -1,31 +1,36 @@
-Imports System.Reflection
-Imports System.Runtime.InteropServices
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
+/*
+ General Information about an assembly is controlled through the following 
+ set of attributes. Change these attribute values to modify the information
+ associated with an assembly.
 
-' Review the values of the assembly attributes
+ Review the values of the assembly attributes
+*/
 
-<Assembly: AssemblyTitle("")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("")> 
-<Assembly: AssemblyCopyright("")> 
-<Assembly: AssemblyTrademark("")> 
-<Assembly: CLSCompliant(True)> 
+[assembly: AssemblyTitle("")] 
+[assembly: AssemblyDescription("")] 
+[assembly: AssemblyCompany("")] 
+[assembly: AssemblyProduct("")] 
+[assembly: AssemblyCopyright("")] 
+[assembly: AssemblyTrademark("")] 
+[assembly: CLSCompliant(true)] 
 
-'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("4772D607-0C91-4FB5-8751-CAC46FC4FA89")> 
+//The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4772D607-0C91-4FB5-8751-CAC46FC4FA89")] 
 
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
+/*
+ Version information for an assembly consists of the following four values:
 
-<Assembly: AssemblyVersion("1.0.*")> 
+      Major Version
+      Minor Version 
+      Build Number
+      Revision
+	
+ You can specify all the values or you can default the Build and Revision Numbers 
+ by using the '*' as shown below:
+*/
+
+[assembly: AssemblyVersion("1.0.*")] 
