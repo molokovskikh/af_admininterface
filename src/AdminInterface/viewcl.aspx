b1c2c7b34d96358554801a8174fc9ea98fcbea73
Kvasov <Kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2006-04-21 07:19:00 +0000

    Код VB переведен в C# Не тестированно

--- src/AdminInterface/viewcl.aspx
+++ src/AdminInterface/viewcl.aspx
@@ -1,50 +1,55 @@
-<%@ Page Language="vb" AutoEventWireup="false" Inherits="AddUser.viewcl" CodeFile="viewcl.aspx.vb" %>
-<HTML>
-	<HEAD>
-		<title>Интерфейс управления клиентами</title>
-		<meta content="True" name="vs_showGrid">
-		<meta content="http://schemas.microsoft.com/intellisense/ie5" name="vs_targetSchema">
-		<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
-	</HEAD>
-	<body vLink="#ab51cc" aLink="#0093e1" link="#0093e1" bgColor="#ffffff">
-		<form id="Form1" method="post" runat="server">
-			<TABLE id="Table1" cellSpacing="0" cellPadding="0" width="95%" align="center" border="0">
-				<TR>
-					<TD align="center"><asp:label id="HeaderLB" runat="server" Font-Names="Verdana" Font-Size="10pt" Font-Bold="True"></asp:label></TD>
-				</TR>
-				<TR>
-					<TD align="center"><FONT face="Verdana" size="2">Клиентов, отвечающих условиям выборки:</FONT>
-						<asp:label id="CountLB" runat="server" Font-Names="Verdana" Font-Size="8pt"></asp:label></TD>
-				</TR>
-				<TR>
-					<TD align="center"><asp:datagrid id=CLList runat="server" Font-Names="Verdana" Font-Size="8pt" CellPadding="0" AutoGenerateColumns="False" BorderColor="#DADADA" PageSize="20" DataSource="<%# DataTable1 %>">
-							<FooterStyle Font-Names="Verdana"></FooterStyle>
-							<AlternatingItemStyle BackColor="#F6F6F6"></AlternatingItemStyle>
-							<ItemStyle HorizontalAlign="Center" BackColor="#EEF8FF"></ItemStyle>
-							<HeaderStyle Font-Size="8pt" Font-Names="Verdana" Font-Bold="True" HorizontalAlign="Center" VerticalAlign="Middle"
-								BackColor="#EBEBEB"></HeaderStyle>
-							<Columns>
-								<asp:TemplateColumn HeaderText="Время">
-									<ItemTemplate>
-										&nbsp;
-										<asp:Label runat="server" Text='<%# FormatDateTime(DataBinder.Eval(Container, "DataItem.LogTime"), DateFormat.LongTime) %>'>
-										</asp:Label>&nbsp;
-									</ItemTemplate>
-									<EditItemTemplate>
-										<asp:TextBox runat="server" Text='<%# DataBinder.Eval(Container, "DataItem.LogTime") %>'>
-										</asp:TextBox>
-									</EditItemTemplate>
-								</asp:TemplateColumn>
-								<asp:HyperLinkColumn DataNavigateUrlField="FirmCode" DataNavigateUrlFormatString="info.aspx?cc={0}" DataTextField="ShortName"
-									HeaderText="Клиент"></asp:HyperLinkColumn>
-								<asp:BoundColumn DataField="Region" SortExpression="Region" HeaderText="Регион"></asp:BoundColumn>
-								<asp:BoundColumn DataField="Addition" HeaderText="Описание"></asp:BoundColumn>
-							</Columns>
-							<PagerStyle VerticalAlign="Middle" Font-Size="9pt" Font-Names="Verdana" Font-Bold="True" HorizontalAlign="Center"
-								Position="TopAndBottom" Mode="NumericPages"></PagerStyle>
-						</asp:datagrid></TD>
-				</TR>
-			</TABLE>
-		</form>
-	</body>
-</HTML>
+<%@ Page Language="c#" AutoEventWireup="true" Inherits="AddUser.viewcl" CodeFile="viewcl.aspx.cs" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Интерфейс управления клиентами</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+</head>
+<body vlink="#ab51cc" alink="#0093e1" link="#0093e1" bgcolor="#ffffff">
+    <form id="Form1" method="post" runat="server">
+        <table id="Table1" cellspacing="0" cellpadding="0" width="95%" align="center" border="0">
+            <tr>
+                <td align="center">
+                    <asp:Label ID="HeaderLB" runat="server" Font-Names="Verdana" Font-Size="10pt" Font-Bold="True"></asp:Label></td>
+            </tr>
+            <tr>
+                <td align="center">
+                    <font face="Verdana" size="2">Клиентов, отвечающих условиям выборки:</font>
+                    <asp:Label ID="CountLB" runat="server" Font-Names="Verdana" Font-Size="8pt"></asp:Label></td>
+            </tr>
+            <tr>
+                <td align="center">
+                    <asp:DataGrid ID="CLList" runat="server" Font-Names="Verdana" Font-Size="8pt" CellPadding="0"
+                        AutoGenerateColumns="False" BorderColor="#DADADA" PageSize="20" DataSource="<%# DataTable1 %>">
+                        <FooterStyle Font-Names="Verdana"></FooterStyle>
+                        <AlternatingItemStyle BackColor="#F6F6F6"></AlternatingItemStyle>
+                        <ItemStyle HorizontalAlign="Center" BackColor="#EEF8FF"></ItemStyle>
+                        <HeaderStyle Font-Size="8pt" Font-Names="Verdana" Font-Bold="True" HorizontalAlign="Center"
+                            VerticalAlign="Middle" BackColor="#EBEBEB"></HeaderStyle>
+                        <Columns>
+                            <asp:TemplateColumn HeaderText="Время">
+                                <ItemTemplate>
+                                    &nbsp;
+                                    <asp:Label runat="server" Text='<%# Convert.ToDateTime(DataBinder.Eval(Container, "DataItem.LogTime")).ToLongDateString() %>'>
+                                    </asp:Label>&nbsp;
+                                </ItemTemplate>
+                                <EditItemTemplate>
+                                    <asp:TextBox runat="server" Text='<%# DataBinder.Eval(Container, "DataItem.LogTime") %>'>
+                                    </asp:TextBox>
+                                </EditItemTemplate>
+                            </asp:TemplateColumn>
+                            <asp:HyperLinkColumn DataNavigateUrlField="FirmCode" DataNavigateUrlFormatString="info.aspx?cc={0}"
+                                DataTextField="ShortName" HeaderText="Клиент"></asp:HyperLinkColumn>
+                            <asp:BoundColumn DataField="Region" SortExpression="Region" HeaderText="Регион"></asp:BoundColumn>
+                            <asp:BoundColumn DataField="Addition" HeaderText="Описание"></asp:BoundColumn>
+                        </Columns>
+                        <PagerStyle VerticalAlign="Middle" Font-Size="9pt" Font-Names="Verdana" Font-Bold="True"
+                            HorizontalAlign="Center" Position="TopAndBottom" Mode="NumericPages"></PagerStyle>
+                    </asp:DataGrid></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
--- src/AdminInterface/viewcl.aspx.cs
+++ src/AdminInterface/viewcl.aspx.cs
@@ -1,168 +1,160 @@
-Imports MySql.Data.MySqlClient
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.Globalization;
+using System.Web.UI;
+using MySql.Data.MySqlClient;
+
+namespace AddUser
+{
+
+	public partial class viewcl : Page
+	{
+
+		protected DataSet DS;
+		protected DataTable DataTable1;
+		protected DataColumn DataColumn1;
+		protected DataColumn DataColumn2;
+		protected DataColumn DataColumn3;
+		protected DataColumn DataColumn4;
+		protected MySqlCommand MyCmd;
+		protected MySqlDataAdapter MyDA;
+		protected MySqlConnection MyCn;
+		protected DataColumn DataColumn5;
+
+		[DebuggerStepThrough()]
+		private void InitializeComponent()
+		{
+			DS = new DataSet();
+			DataTable1 = new DataTable();
+			DataColumn1 = new DataColumn();
+			DataColumn2 = new DataColumn();
+			DataColumn3 = new DataColumn();
+			DataColumn4 = new DataColumn();
+			DataColumn5 = new DataColumn();
+			MyCmd = new MySqlCommand();
+			MyCn = new MySqlConnection();
+			MyDA = new MySqlDataAdapter();
+			DS.BeginInit();
+			DataTable1.BeginInit();
+			// 
+			// DS
+			// 
+			DS.DataSetName = "DS";
+			DS.Locale = new CultureInfo("ru-RU");
+			DS.Tables.AddRange(new DataTable[] { DataTable1 });
+			// 
+			// DataTable1
+			// 
+			DataTable1.Columns.AddRange(new DataColumn[] { DataColumn1, DataColumn2, DataColumn3, DataColumn4, DataColumn5 });
+			DataTable1.TableName = "Table";
+			// 
+			// DataColumn1
+			// 
+			DataColumn1.ColumnName = "LogTime";
+			DataColumn1.DataType = typeof(DateTime);
+			// 
+			// DataColumn2
+			// 
+			DataColumn2.ColumnName = "FirmCode";
+			// 
+			// DataColumn3
+			// 
+			DataColumn3.ColumnName = "ShortName";
+			// 
+			// DataColumn4
+			// 
+			DataColumn4.ColumnName = "Addition";
+			// 
+			// DataColumn5
+			// 
+			DataColumn5.ColumnName = "Region";
+			// 
+			// MyCmd
+			// 
+			MyCmd.CommandText = null;
+			MyCmd.CommandTimeout = 0;
+			MyCmd.CommandType = CommandType.Text;
+			MyCmd.Connection = MyCn;
+			MyCmd.Transaction = null;
+			MyCmd.UpdatedRowSource = UpdateRowSource.Both;
+			// 
+			// MyDA
+			// 
+			MyDA.DeleteCommand = null;
+			MyDA.InsertCommand = null;
+			MyDA.SelectCommand = MyCmd;
+			MyDA.UpdateCommand = null;
+
+			DS.EndInit();
+			DataTable1.EndInit();
+		}
+
+		protected override void OnInit(EventArgs e)
+		{
+			base.OnInit(e);
+			InitializeComponent();
+		}
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (Convert.ToDouble(Session["AccessGrant"]) != 1)
+			{
+				Response.Redirect("default.aspx");
+			}
+
+			ID = Request["id"];
+			MyCn.ConnectionString = Literals.GetConnectionString();
+			MyCn.Open();
+
+			MyCmd.CommandText = " SELECT  Logtime, FirmCode, ShortName, Region, Addition" + " FROM (logs.prgdataex p,  usersettings.clientsdata, accessright.showright, farm.regions r,  usersettings.retclientsset rcs)" + " WHERE p.LogTime>curDate() " + " and rcs.clientcode=p.clientcode " + " and firmcode=p.clientcode " + " and r.regioncode=clientsdata.regioncode " + " and showright.regionmask & maskregion>0";
+
+			if (double.Parse(ID) == 0)
+			{
+				MyCmd.CommandText += " and EXEVersion>0 and UpdateType=5";
+				HeaderLB.Text = ":";
+			}
+
+			if (double.Parse(ID) == 1)
+			{
+				MyCmd.CommandText += " and UpdateType=2";
+				HeaderLB.Text = " :";
+			}
+
+			if (double.Parse(ID) == 2)
+			{
+				MyCmd.CommandText += " and UpdateType=1";
+				HeaderLB.Text = " :";
+			}
+
+			if (double.Parse(ID) == 3)
+			{
+				MyCmd.CommandText += " and EXEVersion>0 and UpdateType=6";
+				HeaderLB.Text = "  :";
+			}
+
+			if (double.Parse(ID) == 4)
+			{
+				MyCmd.CommandText += " and UncommittedUpdateTime>=CURDATE() and UpdateTime<>UncommittedUpdateTime";
+				HeaderLB.Text = "    -  ";
+				CountLB.Text = " ";
+				return;
+			}
+
+			if (double.Parse(ID) == 5)
+			{
+				MyCmd.CommandText += " and UpdateType=3";
+				HeaderLB.Text = ":";
+			}
+			if (double.Parse(ID) <= 5)
+			{
+				MyCmd.CommandText += " and showright.username='" + Session["UserName"] + "'" + " group by p.rowid" + " order by p.logtime desc";
+				CountLB.Text = Convert.ToString(MyDA.Fill(DS, "Table"));
+				CLList.DataBind();
+			}
+			MyCn.Close();
+		}
+	}
 
 
-Namespace AddUser
+}
 
-Partial Class viewcl
-    Inherits System.Web.UI.Page
-
-#Region " Web Form Designer Generated Code "
-
-    'This call is required by the Web Form Designer.
-    <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
-        Me.DS = New System.Data.DataSet
-        Me.DataTable1 = New System.Data.DataTable
-        Me.DataColumn1 = New System.Data.DataColumn
-        Me.DataColumn2 = New System.Data.DataColumn
-        Me.DataColumn3 = New System.Data.DataColumn
-        Me.DataColumn4 = New System.Data.DataColumn
-        Me.DataColumn5 = New System.Data.DataColumn
-        Me.MyCmd = New MySql.Data.MySqlClient.MySqlCommand
-        Me.MyCn = New MySql.Data.MySqlClient.MySqlConnection
-        Me.MyDA = New MySql.Data.MySqlClient.MySqlDataAdapter
-        CType(Me.DS, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.DataTable1, System.ComponentModel.ISupportInitialize).BeginInit()
-        '
-        'DS
-        '
-        Me.DS.DataSetName = "DS"
-        Me.DS.Locale = New System.Globalization.CultureInfo("ru-RU")
-        Me.DS.Tables.AddRange(New System.Data.DataTable() {Me.DataTable1})
-        '
-        'DataTable1
-        '
-        Me.DataTable1.Columns.AddRange(New System.Data.DataColumn() {Me.DataColumn1, Me.DataColumn2, Me.DataColumn3, Me.DataColumn4, Me.DataColumn5})
-        Me.DataTable1.TableName = "Table"
-        '
-        'DataColumn1
-        '
-        Me.DataColumn1.ColumnName = "LogTime"
-        Me.DataColumn1.DataType = GetType(System.DateTime)
-        '
-        'DataColumn2
-        '
-        Me.DataColumn2.ColumnName = "FirmCode"
-        '
-        'DataColumn3
-        '
-        Me.DataColumn3.ColumnName = "ShortName"
-        '
-        'DataColumn4
-        '
-        Me.DataColumn4.ColumnName = "Addition"
-        '
-        'DataColumn5
-        '
-        Me.DataColumn5.ColumnName = "Region"
-        '
-        'MyCmd
-        '
-        Me.MyCmd.CommandText = Nothing
-        Me.MyCmd.CommandTimeout = 0
-        Me.MyCmd.CommandType = System.Data.CommandType.Text
-        Me.MyCmd.Connection = Me.MyCn
-        Me.MyCmd.Transaction = Nothing
-        Me.MyCmd.UpdatedRowSource = System.Data.UpdateRowSource.Both
-        '
-        'MyDA
-        '
-        Me.MyDA.DeleteCommand = Nothing
-        Me.MyDA.InsertCommand = Nothing
-        Me.MyDA.SelectCommand = Me.MyCmd
-        Me.MyDA.UpdateCommand = Nothing
-        CType(Me.DS, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.DataTable1, System.ComponentModel.ISupportInitialize).EndInit()
-
-    End Sub
-    Protected WithEvents DS As System.Data.DataSet
-    Protected WithEvents DataTable1 As System.Data.DataTable
-    Protected WithEvents DataColumn1 As System.Data.DataColumn
-    Protected WithEvents DataColumn2 As System.Data.DataColumn
-    Protected WithEvents DataColumn3 As System.Data.DataColumn
-    Protected WithEvents DataColumn4 As System.Data.DataColumn
-    Protected WithEvents MyCmd As MySql.Data.MySqlClient.MySqlCommand
-    Protected WithEvents MyDA As MySql.Data.MySqlClient.MySqlDataAdapter
-    Protected WithEvents MyCn As MySql.Data.MySqlClient.MySqlConnection
-    Protected WithEvents DataColumn5 As System.Data.DataColumn
-
-
-    Private Sub Page_Init(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Init
-        'CODEGEN: This method call is required by the Web Form Designer
-        'Do not modify it using the code editor.
-        InitializeComponent()
-    End Sub
-
-#End Region
-
-        'Private Sub Page_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-        'End Sub
-
-    Public Function FormatDate(ByVal InputDate As Date, ByVal Enable As Boolean) As String
-        FormatDate = FormatDateTime(InputDate, DateFormat.LongTime)
-    End Function
-
-        Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-            If Session.Item("AccessGrant") <> 1 Then Response.Redirect("default.aspx")
-            'id=0 - Запреты
-            'id=1 - КО
-            'id=2 - Обычные
-            'id=3 - Ошибки
-            'id=4 - В процессе
-            'id=5 - Докачивали
-            ID = Request("id")
-            MyCn.ConnectionString = "Data Source=testsql.analit.net;Database=accessright;User ID=system;Password=123;Connect Timeout=300;Pooling=no"
-            MyCn.Open()
-
-            MyCmd.CommandText = " SELECT  Logtime, FirmCode, ShortName, Region, Addition" & _
-            " FROM (logs.prgdataex p,  usersettings.clientsdata, accessright.showright, farm.regions r,  usersettings.retclientsset rcs)" & _
-            " WHERE p.LogTime>curDate() " & _
-            " and rcs.clientcode=p.clientcode " & _
-            " and firmcode=p.clientcode " & _
-            " and r.regioncode=clientsdata.regioncode " & _
-            " and showright.regionmask & maskregion>0"
-
-            If ID = 0 Then
-                MyCmd.CommandText &= " and EXEVersion>0 and UpdateType=5"
-                HeaderLB.Text = "Запреты:"
-            End If
-
-            If ID = 1 Then
-                MyCmd.CommandText &= " and UpdateType=2"
-                HeaderLB.Text = "Кумулятивные обновления:"
-            End If
-
-            If ID = 2 Then
-                MyCmd.CommandText &= " and UpdateType=1"
-                HeaderLB.Text = "Обычные обновления:"
-            End If
-
-            If ID = 3 Then
-                MyCmd.CommandText &= " and EXEVersion>0 and UpdateType=6"
-                HeaderLB.Text = "Ошибки подготовки данных:"
-            End If
-
-            If ID = 4 Then
-                MyCmd.CommandText &= " and UncommittedUpdateTime>=CURDATE() and UpdateTime<>UncommittedUpdateTime"
-                HeaderLB.Text = "В процессе получения обновления - Нет данных"
-                CountLB.Text = "Нет данных"
-                Exit Sub
-            End If
-
-            If ID = 5 Then
-                MyCmd.CommandText &= " and UpdateType=3"
-                HeaderLB.Text = "Докачки:"
-            End If
-            If ID > 5 Then GoTo exits
-
-            MyCmd.CommandText &= " and showright.username='" & Session("UserName") & "'" & _
-            " group by p.rowid" & _
-            " order by p.logtime desc"
-            CountLB.Text = MyDA.Fill(DS, "Table")
-            CLList.DataBind()
-exits:
-            MyCn.Close()
-        End Sub
-    End Class
-
-End Namespace
