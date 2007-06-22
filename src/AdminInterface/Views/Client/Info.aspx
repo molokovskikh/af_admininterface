dd97641b579caa5026baa64913fc7881a782b3aa
Kvasov <Kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2007-06-22 12:46:00 +0000

    Для отрисовки контактной информации теперь использует вид brail

---
+++ src/AdminInterface/Views/Client/Info.aspx
@@ -0,0 +1,195 @@
+<%@ Page Language="c#" AutoEventWireup="true" Inherits="Views.Client.Info" CodePage="1251"
+	CodeFile="Info.aspx.cs" Theme="Main" %>
+<%@ Import namespace="Castle.MonoRail.Framework"%>
+<%@ Import namespace="Castle.MonoRail.Views.Brail"%>
+<%@ Import namespace="AdminInterface.Components"%>
+<%@ Import namespace="System.IO"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title>Информация о клиентах</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+	<style>
+.InfoRow
+{
+	height: 20px;
+}
+</style>
+</head>
+<body>
+
+	<form id="Form1" method="post" runat="server">
+		<h3>
+			<asp:Label ID="ShortNameLB" runat="server" />
+		</h3>
+		<div class="BorderedBlock" style="padding-top: 20px; padding-bottom: 20px;">
+			<div class="TwoColumn" align="left">
+				<table align="center" border="1" bordercolor="#dadada" cellpadding="0" cellspacing="0"
+					id="Table2" width="90%">
+					<tr>
+						<td colspan="2" height="20">
+							<asp:HyperLink ID="ConfigHL" runat="server">Настройка</asp:HyperLink>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" height="20">
+							<asp:HyperLink ID="BillingLink" runat="server">Биллинг</asp:HyperLink>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" height="20">
+							<asp:HyperLink ID="ChPassHL" runat="server">Статистика изменения пароля</asp:HyperLink>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" height="20">
+							<asp:HyperLink ID="UpdateListHL" runat="server">Статистика обновлений</asp:HyperLink>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" height="20">
+							<asp:HyperLink ID="OrderHistoryHL" runat="server">История заказов</asp:HyperLink>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" height="20">
+							<asp:HyperLink ID="ChPass" runat="server">Изменение пароля</asp:HyperLink>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" height="20">
+							<asp:HyperLink ID="UserInterfaceHL" runat="server">Интерфейс пользователя</asp:HyperLink>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" style="background-color: #f0f8ff; text-align: center; vertical-align: middle; font-weight:bold;">
+							Показываемые клиенты:
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2">
+							<asp:GridView ID="ShowClientsGrid" runat="server" AutoGenerateColumns="False" OnRowCommand="ShowClientsGrid_RowCommand" OnRowDataBound="ShowClientsGrid_RowDataBound" OnRowDeleting="ShowClientsGrid_RowDeleting">
+								<EmptyDataTemplate>
+									<asp:Button ID="AddButton" runat="server" CommandName="Add" Text="Добавить клиента" />
+								</EmptyDataTemplate>
+								<Columns>
+									<asp:TemplateField>
+										<HeaderTemplate>
+											<asp:Button ID="AddButton" CommandName="Add" runat="server" Text="Добавить" />
+										</HeaderTemplate>
+										<ItemTemplate>
+											<asp:Button ID="DeleteButton" CommandName="Delete" runat="server" Text="Удалить" />
+										</ItemTemplate>
+										<ItemStyle Width="10%" />
+									</asp:TemplateField>
+									<asp:TemplateField HeaderText="Клиент">
+										<ItemTemplate>
+											<asp:TextBox ID="SearchText" runat="server" />
+											<asp:Button ID="SearchButton" CommandName="Search" runat="server" Text="Найти" ValidationGroup="3" />
+											<asp:DropDownList ID="ShowClientsList" runat="server" DataTextField="ShortName" DataValueField="FirmCode">
+											</asp:DropDownList>
+											<asp:CustomValidator ID="ShowCleintsValidator" runat="server" ControlToValidate="ShowClientsList"
+												ErrorMessage="Необходимо выбрать клиента." ValidateEmptyText="True" ValidationGroup="1" OnServerValidate="ShowCleintsValidator_ServerValidate" Width="13px">*</asp:CustomValidator>
+										</ItemTemplate>
+									</asp:TemplateField>
+								</Columns>
+							</asp:GridView>
+						</td>
+					</tr>
+					<tr id="UnlockRow" runat="server">
+						<td colspan="2">
+							<asp:Button ID="UnlockButton" runat="server" Text="Разблокировать" OnClick="UnlockButton_Click" ValidationGroup="2" />
+							<asp:Label ID="UnlockedLabel" runat="server" Text="Разблокированно" ForeColor="Green" />
+						</td>
+					</tr>
+					<tr>
+						<td style="background-color: #f0f8ff; text-align: center; vertical-align: middle;
+							height: 30px;" colspan="2">
+							<strong>Общая информация</strong>
+						</td>
+					</tr>
+					<tr class="InfoRow">
+						<td align="right">
+							Полное наименование:
+						</td>
+						<td>
+							<asp:TextBox ID="FullNameText" runat="server" Width="90%" />
+						</td>
+					</tr>
+					<tr class="InfoRow">
+						<td style="text-align: right;">
+							Краткое наименование:
+						</td>
+						<td>
+							<asp:TextBox ID="ShortNameText" runat="server" Width="90%" />
+						</td>
+					</tr>
+					<tr class="InfoRow">
+						<td align="right">
+							Адрес:
+						</td>
+						<td>
+							<asp:TextBox ID="AddressText" runat="server" Width="90%" />
+						</td>
+					</tr>
+					<tr class="InfoRow">
+						<td align="right">
+							Факс:
+						</td>
+						<td align="left">
+							<asp:TextBox ID="FaxText" runat="server" Width="90%" />
+						</td>
+					</tr>
+					<tr class="InfoRow">
+						<td align="right">
+							URL:
+						</td>
+						<td align="left">
+							<asp:TextBox ID="UrlText" runat="server" Width="90%" />
+							<asp:RegularExpressionValidator ID="UrlValidator" runat="server" ErrorMessage="*"
+								ControlToValidate="UrlText" ValidationExpression="http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?"
+								ValidationGroup="1"></asp:RegularExpressionValidator>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2">
+<% 
+	TextWriter textWriter = new StringWriter();
+	Controller.InPlaceRenderView(textWriter, "ContactViewer");
+	Response.Write(textWriter.ToString());		
+%>
+
+						</td>
+					</tr>
+				</table>
+			</div>
+			<div class="TwoColumn" style="text-align: center;">
+				Новое обращение(сообщение):
+				<br />
+				<asp:Label ID="IsMessafeSavedLabel" runat="server" Visible="False" Text="Сохранено"
+					ForeColor="Green"></asp:Label>
+				<asp:TextBox ID="ProblemTB" runat="server" Height="150px" TextMode="MultiLine" Width="90%" />
+				<br />
+				<asp:Button ID="Button1" runat="server" OnClick="Button1_Click"
+					Text="Принять" ValidationGroup="2" />
+				<p>
+					<asp:GridView ID="LogsGrid" runat="server" AutoGenerateColumns="False" DataMember="Logs">
+						<Columns>
+							<asp:BoundField DataField="Date" HeaderText="Дата" />
+							<asp:BoundField DataField="UserName" HeaderText="Оператор" />
+							<asp:BoundField DataField="Message" HeaderText="Событие" />
+						</Columns>
+						<EmptyDataTemplate>
+							Сообщений нет.
+						</EmptyDataTemplate>
+					</asp:GridView>
+				</p>
+			</div>
+		</div>
+		<div class="CopyRight">
+			© АК<a href="http://www.analit.net/">"Инфорум"</a>2005
+		</div>
+	</form>
+</body>
+</html>
---
+++ src/AdminInterface/Views/Client/Info.aspx.cs
@@ -0,0 +1,498 @@
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ActiveDs;
+using AddUser;
+using Castle.MonoRail.Framework;
+using MySql.Data.MySqlClient;
+
+namespace Views.Client
+{
+	partial class Info : Page, IControllerAware
+	{
+		private uint _clientCode
+		{
+			get { return (uint)(Session["ClientCode"] != null ? Session["ClientCode"] : 0u); }
+			set { Session["ClientCode"] = value; }
+		}
+
+		private DataSet _data
+		{
+			get { return (DataSet)Session["InfoDataSet"]; }
+			set { Session["InfoDataSet"] = value; }
+		}
+
+		private MySqlCommand _command = new MySqlCommand();
+		private MySqlConnection _connection = new MySqlConnection(Literals.GetConnectionString());
+		private Controller _controller;
+
+		public Controller Controller
+		{
+			get { return _controller; }
+		}
+
+		private void GetMessages()
+		{
+            try
+            {
+                MySqlDataAdapter logsDataAddapter = new MySqlDataAdapter(
+    @"
+create temporary table Info(Date DateTime, UserName varchar(50), Message text); 
+        INSERT 
+        INTO    info 
+		SELECT	LogTime as Date,
+				OperatorName as UserName,
+				Concat('$$$Изменение УИН: ', ResetIDCause) as Message
+		FROM `Logs`.ret_clients_set_logs
+		WHERE ClientCode=?ClientCode  AND ResetIDCause IS NOT NULL
+		UNION
+        SELECT  WriteTime as Date, 
+                UserName, 
+                Message 
+        FROM    logs.clientsinfo 
+        WHERE   clientcode=?ClientCode 
+        UNION  
+        SELECT  LogTime Date, 
+                OperatorName UserName, 
+                concat('###Сообщение: ', Message, '###, Показов:', ShowMessageCount) Message  
+        FROM    logs.retclientssetupdate 
+        WHERE   Message is not null 
+                AND clientcode=?ClientCode  
+        ORDER BY Date desc; 
+        SELECT * FROM info; 
+        DROP temporary table info;
+", _connection);
+                logsDataAddapter.SelectCommand.Parameters.Add("?UserName", Session["UserName"]);
+                logsDataAddapter.SelectCommand.Parameters.Add("?ClientCode", _clientCode);
+
+                if (_data.Tables["Logs"] != null)
+                    _data.Tables["Logs"].Clear();
+                _connection.Open();
+                logsDataAddapter.SelectCommand.Transaction = _connection.BeginTransaction(IsolationLevel.ReadCommitted);
+                logsDataAddapter.Fill(_data, "Logs");
+                logsDataAddapter.SelectCommand.Transaction.Commit();
+            }
+            finally
+            {
+                _connection.Close();
+            }
+		}
+
+		protected void Button1_Click(object sender, EventArgs e)
+		{
+            try
+            {
+                _connection.Open();
+                _command.Connection = _connection;
+                _command.Transaction = _connection.BeginTransaction(IsolationLevel.RepeatableRead);
+                _command.CommandText =
+@"
+SET @InHost = ?UserHost;
+Set @InUser = ?UserName;
+
+INSERT 
+INTO    logs.clientsinfo VALUES
+        (
+                null, 
+                ?UserName, 
+                now(), 
+                ?ClientCode, 
+                ?Problem
+        );
+";
+                _command.Parameters.Add("?Problem", ProblemTB.Text);
+                _command.Parameters.Add("?UserName", Session["UserName"]);
+                _command.Parameters.Add("?UserHost", HttpContext.Current.Request.UserHostAddress);
+                _command.Parameters.Add("?ClientCode", _clientCode);
+
+                _command.ExecuteNonQuery();
+                _command.Transaction.Commit();
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+			ProblemTB.Text = String.Empty;
+			IsMessafeSavedLabel.Visible = true;
+			UnlockRow.Visible = false;
+			
+			GetMessages();
+			LogsGrid.DataSource = _data;
+			LogsGrid.DataBind();
+		}
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+#if !DEBUG
+			if (Convert.ToInt32(Session["AccessGrant"]) != 1)
+				Response.Redirect("default.aspx");
+#endif
+
+			if (!IsPostBack)
+			{
+				_clientCode = Convert.ToUInt32(Request["cc"]);
+
+				OrderHistoryHL.NavigateUrl = "orders.aspx?cc=" + _clientCode;
+				OrderHistoryHL.NavigateUrl = "orders.aspx?cc=" + _clientCode;
+				UpdateListHL.NavigateUrl = "updates.aspx?cc=" + _clientCode;
+				ChPass.NavigateUrl = "chpassgo.aspx?cc=" + _clientCode + "&ouar=" + Request["ouar"];
+				ChPass.Enabled = Convert.ToBoolean(Session["ChPass"]);
+				UserInterfaceHL.NavigateUrl = "https://stat.analit.net/ci/auth/logon.aspx?sid=" + _clientCode;
+				
+				GetData();
+				ConnectDataSource();
+				DataBind();
+			}
+		}
+
+		private void ConnectDataSource()
+		{
+			LogsGrid.DataSource = _data.Tables["Logs"];
+			ShowClientsGrid.DataSource = _data.Tables["ShowClients"];
+
+			FullNameText.Text = _data.Tables["Info"].Rows[0]["FullName"].ToString();
+			ShortNameText.Text = _data.Tables["Info"].Rows[0]["ShortName"].ToString();
+			ShortNameLB.Text = _data.Tables["Info"].Rows[0]["ShortName"].ToString();
+//			PhoneText.Text = _data.Tables["Info"].Rows[0]["Phone"].ToString();
+//			SetSipLink(PhoneText, PhoneText.Text);
+			AddressText.Text = _data.Tables["Info"].Rows[0]["Adress"].ToString();
+			FaxText.Text = _data.Tables["Info"].Rows[0]["Fax"].ToString();
+//			EmailText.Text = _data.Tables["Info"].Rows[0]["Mail"].ToString();
+//			SetEmailLink(EmailText);
+			UrlText.Text = _data.Tables["Info"].Rows[0]["URL"].ToString();
+
+//			OrderManagerPhoneText.Text = _data.Tables["Info"].Rows[0]["OrderManagerPhone"].ToString();
+//			SetSipLink(OrderManagerPhoneText, OrderManagerPhoneText.Text);
+//			SetEmailLink(OrderManagerEmailText);
+			
+
+//			ClientManagerPhoneText.Text = _data.Tables["Info"].Rows[0]["ClientManagerPhone"].ToString();
+//			SetSipLink(ClientManagerPhoneText, ClientManagerPhoneText.Text);
+//			ClientManagerEmailText.Text = _data.Tables["Info"].Rows[0]["ClientManagerMail"].ToString();
+//			SetEmailLink(ClientManagerEmailText);
+			
+
+//			AccountantEmailText.Text = _data.Tables["Info"].Rows[0]["AccountantMail"].ToString();
+//			SetEmailLink(AccountantEmailText);
+//			AccountantPhoneText.Text = _data.Tables["Info"].Rows[0]["AccountantPhone"].ToString();
+//			SetSipLink(AccountantPhoneText, AccountantPhoneText.Text);
+			
+
+			UserInterfaceHL.Enabled = Convert.ToBoolean(_data.Tables["Info"].Rows[0]["AlowInterface"]);
+			UpdateListHL.Enabled = Convert.ToBoolean(_data.Tables["Info"].Rows[0]["FirmType"]);
+			if (Convert.ToInt32(_data.Tables["Info"].Rows[0]["FirmType"]) == 1)
+				ConfigHL.NavigateUrl = "manageret";
+			else
+				ConfigHL.NavigateUrl = "managep";
+			ConfigHL.NavigateUrl += ".aspx?cc=" + _clientCode;
+			BillingLink.NavigateUrl = String.Format("Billing/edit.rails?ClientCode={0}", 
+													_clientCode);
+		}
+
+		private static void SetSipLink(HyperLink button, string phoneNumber)
+        {
+            if (!String.IsNullOrEmpty(phoneNumber))
+            {
+                button.Visible = true;
+                button.NavigateUrl = String.Format("sip:8{0}", phoneNumber.Replace("-", ""));
+            }
+            else
+                button.Visible = false;
+        }
+
+		private static void SetEmailLink(HyperLink hyperLink)
+		{
+			if (!String.IsNullOrEmpty(hyperLink.Text))
+			{
+				hyperLink.Visible = true;
+				hyperLink.NavigateUrl = String.Format("mailto:{0}", hyperLink.Text);
+			}
+			else
+				hyperLink.Visible = false;
+		}
+
+		private void GetData()
+		{
+            _data = new DataSet();
+
+            GetMessages();
+
+            MySqlDataAdapter infoDataAdapter = new MySqlDataAdapter(
+@"
+SELECT  cd.FullName,   
+        cd.ShortName,   
+        cd.Phone,   
+        cd.Adress,   
+        cd.Fax,   
+        cd.Mail,   
+        cd.URL,   
+        cd.OrderManagerName,   
+        cd.OrderManagerPhone,   
+        cd.OrderManagerMail,   
+        cd.ClientManagerName,   
+        cd.ClientManagerPhone,   
+        cd.ClientManagerMail,   
+        cd.AccountantName,   
+        cd.AccountantPhone,   
+        cd.AccountantMail,   
+        (if(regionaladmins.UseRegistrant                =1, Registrant=?UserName, 1=1))   
+        AND (regionaladmins.regionmask & cd.regioncode  >0)   
+        AND (if(AlowRetailInterface+AlowVendorInterface =2, 1=1, if(Alowretailinterface=1, firmtype=Alowretailinterface, if(AlowVendorInterface=1, firmtype=0, 0)))) as AlowInterface,   
+        FirmType,   
+        ouar.OsUserName  
+FROM    (clientsdata cd,   accessright.regionaladmins)     
+LEFT JOIN OsUserAccessRight ouar 
+        ON ouar.ClientCode                       = cd.FirmCode  
+WHERE   FirmType                                 =if(ShowRetail + ShowVendor=2, FirmType, if(ShowRetail = 1, 1, 0))   
+        AND cd.regioncode & regionaladmins.regionmask > 0   
+        AND if(regionaladmins.UseRegistrant      =1, Registrant=?UserName, 1=1)   
+        AND firmcode                             =?ClientCode   
+        AND regionaladmins.username              =?UserName;
+", _connection);
+            infoDataAdapter.SelectCommand.Parameters.Add("?UserName", Session["UserName"]);
+            infoDataAdapter.SelectCommand.Parameters.Add("?ClientCode", _clientCode);
+				
+			MySqlDataAdapter showClientsAdapter = new MySqlDataAdapter(
+@"
+SELECT  DISTINCT cd.FirmCode, 
+        convert(concat(cd.FirmCode, '. ', cd.ShortName) using cp1251) ShortName     
+FROM    (accessright.regionaladmins, clientsdata as cd) 
+	JOIN ShowRegulation sr ON sr.ShowClientCode = cd.FirmCode
+WHERE   sr.PrimaryClientCode				 = ?ClientCode
+		AND cd.regioncode & regionaladmins.regionmask > 0 
+        AND regionaladmins.UserName               = ?UserName 
+        AND FirmType                         = if(ShowRetail+ShowVendor = 2, FirmType, if(ShowRetail = 1, 1, 0)) 
+        AND if(UseRegistrant                 = 1, Registrant = ?UserName, 1 = 1)   
+        AND FirmStatus    = 1 
+        AND billingstatus = 1 
+ORDER BY cd.shortname;
+", _connection);
+
+			showClientsAdapter.SelectCommand.Parameters.Add("?ClientCode", _clientCode);
+			showClientsAdapter.SelectCommand.Parameters.Add("?UserName", Session["UserName"]);
+            
+            try
+            {
+                _connection.Open();
+                MySqlTransaction transaction = _connection.BeginTransaction(IsolationLevel.ReadCommitted);
+                showClientsAdapter.SelectCommand.Transaction = transaction;
+                infoDataAdapter.SelectCommand.Transaction = transaction;
+                showClientsAdapter.Fill(_data, "ShowClients");
+                infoDataAdapter.Fill(_data, "Info");
+                transaction.Commit();
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            if (_data.Tables["Info"].Rows[0]["OsUserName"] != DBNull.Value || !String.IsNullOrEmpty(_data.Tables["Info"].Rows[0]["OsUserName"].ToString()))
+            {
+				try
+				{
+					IADsUser ADUser = Marshal.BindToMoniker("WinNT://adc.analit.net/" + _data.Tables["Info"].Rows[0]["OsUserName"]) as IADsUser;
+					UnlockRow.Visible = ADUser.IsAccountLocked;
+					UnlockedLabel.Visible = false;
+				}
+				catch
+				{
+					UnlockRow.Visible = false;
+				}
+            }
+            else
+                UnlockRow.Visible = false;
+
+		}
+
+		protected void SaveButton_Click(object sender, EventArgs e)
+		{
+			MySqlTransaction transaction = null;
+			try
+			{
+				ProcessChanges();
+				_connection.Open();
+				transaction = _connection.BeginTransaction(IsolationLevel.RepeatableRead);
+
+				_command.Connection = _connection;
+				_command.CommandText = @"
+SET @InHost = ?UserHost;
+Set @InUser = ?UserName;
+
+UPDATE ClientsData
+SET FullName = ?FullName,
+	ShortName = ?ShortName,
+	Adress = ?Address,
+	Fax = ?Fax,
+	URL = ?Url, 
+	OrderManagerName = ?OrderManagerName,
+	ClientManagerName = ?ClientManagerName,
+	AccountantName = ?AccountantName
+WHERE firmcode = ?ClientCode  
+";
+				_command.Parameters.Add("?ClientCode", _clientCode);
+				_command.Parameters.Add("?FullName", FullNameText.Text);
+				_command.Parameters.Add("?ShortName", ShortNameText.Text);
+				_command.Parameters.Add("?Address", AddressText.Text);
+				_command.Parameters.Add("?Fax", FaxText.Text);
+				_command.Parameters.Add("?Url", UrlText.Text);
+				_command.Parameters.Add("?UserName", Session["UserName"]);
+				_command.Parameters.Add("?UserHost", HttpContext.Current.Request.UserHostAddress);
+				_command.ExecuteNonQuery();
+
+				MySqlDataAdapter showClientsAdapter = new MySqlDataAdapter();
+				showClientsAdapter.DeleteCommand = new MySqlCommand(@"
+DELETE FROM showregulation 
+WHERE PrimaryClientCode = ?PrimaryClientCode AND ShowClientCode = ?ShowClientCode;
+", _connection);
+				showClientsAdapter.DeleteCommand.Parameters.Add("?PrimaryClientCode", _clientCode);
+				showClientsAdapter.DeleteCommand.Parameters.Add("?ShowClientCode", MySqlDbType.Int32, 0, "FirmCode");
+
+				showClientsAdapter.InsertCommand = new MySqlCommand(
+	@"
+INSERT INTO showregulation 
+SET PrimaryClientCode = ?PrimaryClientCode,
+	ShowClientCode = ?ShowClientCode;
+", _connection);
+				showClientsAdapter.InsertCommand.Parameters.Add("?PrimaryClientCode", _clientCode);
+				showClientsAdapter.InsertCommand.Parameters.Add("?ShowClientCode", MySqlDbType.Int32, 0, "FirmCode");
+
+				showClientsAdapter.Update(_data, "ShowClients");
+				_data.Tables["ShowClients"].AcceptChanges();
+
+				transaction.Commit();
+				
+				IsMessafeSavedLabel.Visible = false;
+				UnlockRow.Visible = false;
+			}
+			catch (Exception ex)
+			{ 
+				if (transaction != null)
+					transaction.Rollback();
+				throw new Exception("Ошибка на странице Info.aspx", ex);
+			}
+			finally
+			{
+				_connection.Close();
+			}
+		}
+
+		protected void UnlockButton_Click(object sender, EventArgs e)
+		{
+			IADsUser ADUser = Marshal.BindToMoniker("WinNT://adc.analit.net/" + _data.Tables["Info"].Rows[0]["OsUserName"]) as IADsUser;
+			ADUser.IsAccountLocked = false;
+			ADUser.SetInfo();
+			UnlockRow.Visible = true;
+			UnlockButton.Visible = false;
+			UnlockedLabel.Visible = true;
+		}
+
+		protected void ShowClientsGrid_RowDataBound(object sender, GridViewRowEventArgs e)
+		{
+			if (e.Row.RowType == DataControlRowType.DataRow)
+			{
+				DataRowView rowView = (DataRowView)e.Row.DataItem;
+				if (rowView.Row.RowState == DataRowState.Added)
+				{
+					((Button)e.Row.FindControl("SearchButton")).CommandArgument = e.Row.RowIndex.ToString();
+					((Button)e.Row.FindControl("SearchButton")).Visible = true;
+					((TextBox)e.Row.FindControl("SearchText")).Visible = true;
+				}
+				else
+				{
+					((Button)e.Row.FindControl("SearchButton")).Visible = false;
+					((TextBox)e.Row.FindControl("SearchText")).Visible = false;
+				}
+				DropDownList list = ((DropDownList)e.Row.FindControl("ShowClientsList"));
+				list.Items.Add(new ListItem(rowView["ShortName"].ToString(), rowView["FirmCode"].ToString()));
+				list.Width = new Unit(90, UnitType.Percentage);
+			}
+		}
+
+		protected void ShowClientsGrid_RowCommand(object sender, GridViewCommandEventArgs e)
+		{
+			switch (e.CommandName)
+			{
+				case "Add":
+					ProcessChanges();
+					_data.Tables["ShowClients"].Rows.Add(_data.Tables["ShowClients"].NewRow());
+					ShowClientsGrid.DataSource = _data.Tables["ShowClients"].DefaultView;
+					ShowClientsGrid.DataBind();
+					break;
+				case "Search":
+					MySqlDataAdapter adapter = new MySqlDataAdapter
+(@"
+SELECT  DISTINCT cd.FirmCode, 
+        convert(concat(cd.FirmCode, '. ', cd.ShortName) using cp1251) ShortName
+FROM    (accessright.regionaladmins, clientsdata as cd)  
+LEFT JOIN showregulation sr
+        ON sr.ShowClientCode	             =cd.firmcode  
+WHERE   cd.regioncode & regionaladmins.regionmask > 0  
+        AND regionaladmins.UserName               =?UserName  
+        AND FirmType                         =if(ShowRetail+ShowVendor=2, FirmType, if(ShowRetail=1, 1, 0)) 
+        AND if(UseRegistrant                 =1, Registrant=?UserName, 1=1)  
+        AND cd.ShortName like ?SearchText
+        AND FirmStatus   =1  
+        AND billingstatus=1  
+ORDER BY cd.shortname;
+", Literals.GetConnectionString());
+					adapter.SelectCommand.Parameters.Add("?UserName", Session["UserName"]);
+					adapter.SelectCommand.Parameters.Add("?SearchText", string.Format("%{0}%", ((TextBox)ShowClientsGrid.Rows[Convert.ToInt32(e.CommandArgument)].FindControl("SearchText")).Text));
+
+					DataSet data = new DataSet();
+                    try
+                    {
+                        _connection.Open();
+                        adapter.SelectCommand.Transaction = _connection.BeginTransaction(IsolationLevel.ReadCommitted);
+                        adapter.Fill(data);
+                        adapter.SelectCommand.Transaction.Commit();
+                    }
+                    finally
+                    {
+                        _connection.Close();
+                    }
+
+					DropDownList ShowList = ((DropDownList)ShowClientsGrid.Rows[Convert.ToInt32(e.CommandArgument)].FindControl("ShowClientsList"));
+					ShowList.DataSource = data;
+					ShowList.DataBind();
+					ShowList.Visible = data.Tables[0].Rows.Count > 0;
+					break;
+			}
+		}
+
+		protected void ShowClientsGrid_RowDeleting(object sender, GridViewDeleteEventArgs e)
+		{
+			ProcessChanges();
+			_data.Tables["ShowClients"].DefaultView[e.RowIndex].Delete();
+			ShowClientsGrid.DataSource = _data.Tables["ShowClients"].DefaultView;
+			ShowClientsGrid.DataBind();
+		}
+
+		private void ProcessChanges()
+		{
+			foreach (GridViewRow row in ShowClientsGrid.Rows)
+			{
+				if (_data.Tables["ShowClients"].DefaultView[row.RowIndex]["ShortName"].ToString() != ((DropDownList)row.FindControl("ShowClientsList")).SelectedItem.Text)
+				{
+					_data.Tables["ShowClients"].DefaultView[row.RowIndex]["ShortName"] = ((DropDownList)row.FindControl("ShowClientsList")).SelectedItem.Text;
+					_data.Tables["ShowClients"].DefaultView[row.RowIndex]["FirmCode"] = ((DropDownList)row.FindControl("ShowClientsList")).SelectedValue;
+				}
+			}
+		}
+		protected void ShowCleintsValidator_ServerValidate(object source, ServerValidateEventArgs args)
+		{
+			args.IsValid = !String.IsNullOrEmpty(args.Value);
+		}
+
+		public void SetController(Controller controller)
+		{
+			_controller = controller;
+		}
+	}
+}
