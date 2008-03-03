68dcd3d5090ae09efacfc9186b146dcaebc40ac0
Kvasov <Kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2008-03-03 09:27:00 +0000

    Небольшой рефакторинг

--- src/AdminInterface/report.aspx.cs
+++ src/AdminInterface/report.aspx.cs
@@ -12,9 +12,8 @@ namespace AddUser
 		protected void Page_Load(object sender, EventArgs e) 
 		{
 			if (Convert.ToInt32(Session["AccessGrant"]) != 1)
-			{
 				Response.Redirect("default.aspx");
-			}
+
 			string str = Session["strStatus"].ToString();
 			string Code = Session["Code"].ToString();
 			string DogN = Session["DogN"].ToString();
@@ -24,33 +23,33 @@ namespace AddUser
 			string Password = Session["Password"].ToString();
 			bool IsRegister = Convert.ToBoolean(Session["Register"]);
 			string Tariff = Session["Tariff"].ToString();
-			ChPassMessLB.Visible = !(IsRegister);
-			RepLb.Visible = !(IsRegister);
-			if (str == "Yes")
-			{
-				LBClient.Text = Name;
-				LBCCard.Text = Name;
-				LBShortName.Text = ShortName;
-				LBLogin.Text = Login;
-				LBLcard.Text = Login;
-				LBPassword.Text = Password;
-				LBCode.Text = Code;
-				LBCard.Text = Code;
-				DogNLB.Text = DogN;
-				DogNNLB.Text = DogN;
-				TariffLB.Text = Tariff;
-				TariffD.Text = Tariff;
-				LBDate.Text = DateTime.Now.ToString();
-				RegDate.Text = DateTime.Now.ToString();
-				if ((LBClient.Text == "") & (LBLogin.Text == "") & (LBPassword.Text == ""))
-				{
-					Server.Transfer("default.aspx");
-				}
-			}
-			else
+			ChPassMessLB.Visible = !IsRegister;
+			RepLb.Visible = !IsRegister;
+			if (str != "Yes")
 			{
 				Server.Transfer("default.aspx");
+				return;
 			}
+
+			LBClient.Text = Name;
+			LBCCard.Text = Name;
+			LBShortName.Text = ShortName;
+			LBLogin.Text = Login;
+			LBLcard.Text = Login;
+			LBPassword.Text = Password;
+			LBCode.Text = Code;
+			LBCard.Text = Code;
+			DogNLB.Text = DogN;
+			DogNNLB.Text = DogN;
+			TariffLB.Text = Tariff;
+			TariffD.Text = Tariff;
+			LBDate.Text = DateTime.Now.ToString();
+			RegDate.Text = DateTime.Now.ToString();
+
+			if (LBClient.Text == ""
+				&& LBLogin.Text == ""
+				&& LBPassword.Text == "")
+				Server.Transfer("default.aspx");
 		}
 	}
 }
