392495f5ba9b436daa25f52a51aa4bed173211f4
Kvasov <Kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2008-03-03 12:47:00 +0000

    Adm-946

---
+++ src/AdminInterface/Models/MailSentEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Castle.ActiveRecord;
+using NHibernate.Expression;
+
+namespace AdminInterface.Models
+{
+	[ActiveRecord(Table = "billing.MailSentHistory")]
+	public class MailSentEntity : ActiveRecordBase<MailSentEntity>
+	{
+		[PrimaryKey]
+		public uint Id { get; set; }
+
+		[Property]
+		public DateTime SentDate { get; set; }
+
+		[Property]
+		public string UserName { get; set; }
+
+		[Property]
+		public uint PayerId { get; set; }
+
+		[Property]
+		public string Comment { get; set; }
+
+		public static MailSentEntity[] GetHistory(uint payerId)
+		{
+			return ActiveRecordMediator<MailSentEntity>.FindAll(new[] {Order.Desc("SentDate")},
+			                                                    Expression.Eq("PayerId", payerId));
+		}
+	}
+}
