d3aefade3464c4f92daf3fb46883a752fd32d180
Kvasov <Kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2008-04-14 06:47:00 +0000

    Тесты для ADHelper

---
+++ src/AdminInterface.Test/Helpers/ADHelperFixture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AdminInterface.Helpers;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace AdminInterface.Test.Helpers
+{
+	[TestFixture]
+	public class ADHelperFixture
+	{
+		[Test]
+		public void IsLoginExistsTest()
+		{
+			Assert.That(ADHelper.IsLoginExists("kvasov"), Is.True);
+			Assert.That(ADHelper.IsLoginExists("loasdvhuq34y89rhawf"), Is.False);
+		}
+
+		[Test]
+		public void GetPasswordExpirationDate()
+		{
+			Assert.That(ADHelper.GetPasswordExpirationDate("kvasov"), Is.GreaterThan(DateTime.Now));
+		}
+
+		[Test]
+		public void IsLockedTest()
+		{
+			Assert.That(ADHelper.IsLocked("kvasov"), Is.False);
+		}
+
+		[Test]
+		public void IsDisabledTest()
+		{
+			Assert.That(ADHelper.IsDisabled("kvasov"), Is.False);
+		}
+	}
+}
