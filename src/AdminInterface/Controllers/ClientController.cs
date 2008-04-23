bff15ff236105d3f99f2de5a6e4a5ff5c82b4902
Kvasov <Kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2008-04-23 15:11:00 +0000

    Adm-973

--- src/AdminInterface/Controllers/ClientController.cs
+++ src/AdminInterface/Controllers/ClientController.cs
@@ -1,10 +1,11 @@
 using AdminInterface.Filters;
+using AdminInterface.Helpers;
 using Castle.MonoRail.Framework;
 using Common.Web.Ui.Models;
 
 namespace AdminInterface.Controllers
 {
-	[Filter(ExecuteEnum.BeforeAction, typeof(AuthorizeFilter))]
+	[Filter(ExecuteEnum.BeforeAction, typeof(AuthorizeFilter)), Helper(typeof(ADHelper))]
 	public class ClientController : SmartDispatcherController
 	{
 		public void Info(uint cc)
