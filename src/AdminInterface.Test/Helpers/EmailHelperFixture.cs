30f8024d24cd7185f5ed607823dcab88cd400150
Kvasov <Kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2008-05-07 14:40:00 +0000

    тест на обрезание всякой фигни из email

---
+++ src/AdminInterface.Test/Helpers/EmailHelperFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+using AdminInterface.Helpers;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace AdminInterface.Test.Helpers
+{
+	[TestFixture]
+	public class EmailHelperFixture
+	{
+		[Test]
+		public void BuildAttachementFromStringTest()
+		{
+			var message = new MailMessage();
+			EmailHelper.BuildAttachementFromString(@"
+r.kvasov@analit.net
+
+",message);
+			Assert.That(message.To.Count, Is.EqualTo(1), "количество записей в поле to меньше одной");
+		}
+	}
+}
