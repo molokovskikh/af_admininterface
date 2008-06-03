6ad1d2bc868ab7198ac77cb62515d6849110b743
Kvasov <Kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2008-06-03 12:09:00 +0000

    Добавлен layout для формы регистрации биллинговой информации

--- src/AdminInterface/Controllers/RegisterController.cs
+++ src/AdminInterface/Controllers/RegisterController.cs
@@ -1,11 +1,14 @@
-﻿using AdminInterface.Models;
+﻿using AdminInterface.Helpers;
+using AdminInterface.Models;
 using AdminInterface.Security;
 using Castle.MonoRail.ActiveRecordSupport;
 using Castle.MonoRail.Framework;
+using Common.Web.Ui.Helpers;
 using Common.Web.Ui.Models;
 
 namespace AdminInterface.Controllers
 {
+	[Layout("billing"), Helper(typeof(BindingHelper)), Helper(typeof(ViewHelper))]
 	[Security(PermissionType.RegisterDrugstore, PermissionType.RegisterSupplier, Required = Required.AnyOf)]
 	public class RegisterController : SmartDispatcherController
 	{
