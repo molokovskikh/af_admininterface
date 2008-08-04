56d53ef72cfe9f57c8f2859ceb7889f93d0b006b
Kvasov <Kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2008-08-04 11:27:00 +0000

    Заскоментировано то что было закоментировано для дебага

--- src/AdminInterface/NHibernateExtentions/AggregateProjection2.cs
+++ src/AdminInterface/NHibernateExtentions/AggregateProjection2.cs
@@ -1,62 +1,62 @@
-//using System;
-//using System.Collections.Generic;
-//using NHibernate;
-//using NHibernate.Criterion;
-//using NHibernate.SqlCommand;
-//using NHibernate.Type;
-//using NHibernate.Util;
+using System;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Criterion;
+using NHibernate.SqlCommand;
+using NHibernate.Type;
+using NHibernate.Util;
 
-//namespace AdminInterface.NHibernateExtentions
-//{
-//    [Serializable]
-//    public class AggregateProjection2 : SimpleProjection
-//    {
-//        protected readonly IProjection propertyName;
-//        protected readonly string aggregate;
+namespace AdminInterface.NHibernateExtentions
+{
+	[Serializable]
+	public class AggregateProjection2 : SimpleProjection
+	{
+		protected readonly IProjection propertyName;
+		protected readonly string aggregate;
 
-//        public AggregateProjection2(string aggregate, IProjection projection)
-//        {
-//            this.aggregate = aggregate;
-//            this.propertyName = projection;
-//        }
+		public AggregateProjection2(string aggregate, IProjection projection)
+		{
+			this.aggregate = aggregate;
+			this.propertyName = projection;
+		}
 
-//        public override bool IsAggregate
-//        {
-//            get { return true; }
-//        }
+		public override bool IsAggregate
+		{
+			get { return true; }
+		}
 
-//        public override string ToString()
-//        {
-//            return aggregate + "(" + propertyName + ')';
-//        }
+		public override string ToString()
+		{
+			return aggregate + "(" + propertyName + ')';
+		}
 
-//        public override IType[] GetTypes(ICriteria criteria, ICriteriaQuery criteriaQuery)
-//        {
-//            return propertyName.GetTypes(criteria, criteriaQuery);
-//        }
+		public override IType[] GetTypes(ICriteria criteria, ICriteriaQuery criteriaQuery)
+		{
+			return propertyName.GetTypes(criteria, criteriaQuery);
+		}
 
-//        public override SqlString ToSqlString(ICriteria criteria, int loc, ICriteriaQuery criteriaQuery, IDictionary<string, IFilter> enabledFilters)
-//        {
-//            return new SqlString()
-//                .Append(aggregate)
-//                .Append("(")
-//                .Append(StringHelper.RemoveAsAliasesFromSql(propertyName.ToSqlString(criteria, loc, criteriaQuery, enabledFilters)))
-//                .Append(")")
-//                .Append(" as y")
-//                .Append(loc.ToString())
-//                .Append("_");
+		public override SqlString ToSqlString(ICriteria criteria, int loc, ICriteriaQuery criteriaQuery, IDictionary<string, IFilter> enabledFilters)
+		{
+			return new SqlString()
+				.Append(aggregate)
+				.Append("(")
+				.Append(StringHelper.RemoveAsAliasesFromSql(propertyName.ToSqlString(criteria, loc, criteriaQuery, enabledFilters)))
+				.Append(")")
+				.Append(" as y")
+				.Append(loc.ToString())
+				.Append("_");
 
-//        }
+		}
 
-//        public override bool IsGrouped
-//        {
-//            get { return false; }
-//        }
+		public override bool IsGrouped
+		{
+			get { return false; }
+		}
 
-//        public override SqlString ToGroupSqlString(ICriteria criteria, ICriteriaQuery criteriaQuery,
-//                                                   IDictionary<string, IFilter> enabledFilters)
-//        {
-//            throw new InvalidOperationException("not a grouping projection");
-//        }
-//    }
-//}
+		public override SqlString ToGroupSqlString(ICriteria criteria, ICriteriaQuery criteriaQuery,
+												   IDictionary<string, IFilter> enabledFilters)
+		{
+			throw new InvalidOperationException("not a grouping projection");
+		}
+	}
+}
--- src/AdminInterface/NHibernateExtentions/BitOrProjection.cs
+++ src/AdminInterface/NHibernateExtentions/BitOrProjection.cs
@@ -1,55 +1,55 @@
-//using System;
-//using System.Collections.Generic;
-//using NHibernate;
-//using NHibernate.Criterion;
-//using NHibernate.SqlCommand;
-//using NHibernate.Type;
-//using NHibernate.Util;
+using System;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Criterion;
+using NHibernate.SqlCommand;
+using NHibernate.Type;
+using NHibernate.Util;
 
-//namespace AdminInterface.Models
-//{
-//    public class BitOrProjection : SimpleProjection
-//    {
-//        protected readonly IProjection left;
-//        protected readonly IProjection right;
+namespace AdminInterface.Models
+{
+	public class BitOrProjection : SimpleProjection
+	{
+		protected readonly IProjection left;
+		protected readonly IProjection right;
 
-//        public BitOrProjection(IProjection left, IProjection right)
-//        {
-//            this.right = right;
-//            this.left = left;
-//        }
+		public BitOrProjection(IProjection left, IProjection right)
+		{
+			this.right = right;
+			this.left = left;
+		}
 
-//        public override bool IsAggregate
-//        {
-//            get { return false; }
-//        }
+		public override bool IsAggregate
+		{
+			get { return false; }
+		}
 
-//        public override bool IsGrouped
-//        {
-//            get { return false; }
-//        }
+		public override bool IsGrouped
+		{
+			get { return false; }
+		}
 
-//        public override IType[] GetTypes(ICriteria criteria, ICriteriaQuery criteriaQuery)
-//        {
-//            return new IType[] { NHibernateUtil.Int32 };
-//        }
+		public override IType[] GetTypes(ICriteria criteria, ICriteriaQuery criteriaQuery)
+		{
+			return new IType[] { NHibernateUtil.Int32 };
+		}
 
-//        public override SqlString ToSqlString(ICriteria criteria, int loc, ICriteriaQuery criteriaQuery, IDictionary<string, IFilter> enabledFilters)
-//        {
-//            return new SqlString()
-//                .Append(StringHelper.RemoveAsAliasesFromSql(left.ToSqlString(criteria, loc, criteriaQuery, enabledFilters)))
-//                .Append(" & ")
-//                .Append(StringHelper.RemoveAsAliasesFromSql(right.ToSqlString(criteria, loc, criteriaQuery, enabledFilters)))
-//                .Append(" as y")
-//                .Append(loc.ToString())
-//                .Append("_");
+		public override SqlString ToSqlString(ICriteria criteria, int loc, ICriteriaQuery criteriaQuery, IDictionary<string, IFilter> enabledFilters)
+		{
+			return new SqlString()
+				.Append(StringHelper.RemoveAsAliasesFromSql(left.ToSqlString(criteria, loc, criteriaQuery, enabledFilters)))
+				.Append(" & ")
+				.Append(StringHelper.RemoveAsAliasesFromSql(right.ToSqlString(criteria, loc, criteriaQuery, enabledFilters)))
+				.Append(" as y")
+				.Append(loc.ToString())
+				.Append("_");
 
-//        }
+		}
 
-//        public override SqlString ToGroupSqlString(ICriteria criteria, ICriteriaQuery criteriaQuery,
-//                                                   IDictionary<string, IFilter> enabledFilters)
-//        {
-//            throw new InvalidOperationException("BitOrProjection not a grouping projection");
-//        }
-//    }
-//}
+		public override SqlString ToGroupSqlString(ICriteria criteria, ICriteriaQuery criteriaQuery,
+												   IDictionary<string, IFilter> enabledFilters)
+		{
+			throw new InvalidOperationException("BitOrProjection not a grouping projection");
+		}
+	}
+}
