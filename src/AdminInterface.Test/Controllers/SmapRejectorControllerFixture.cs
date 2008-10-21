6c324a3b889a73462beaf550dd0c54c1c87fdf07
kvasov <kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2008-10-21 08:47:45 +0000

    Добавлены тесты для работы с логами спаморезки

---
+++ src/AdminInterface.Test/Controllers/SmapRejectorControllerFixture.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using AdminInterface.Controllers;
+using AdminInterface.Models.Logs;
+using AdminInterface.Models.Security;
+using AdminInterface.Security;
+using AdminInterface.Test.ForTesting;
+using Castle.MonoRail.TestSupport;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace AdminInterface.Test.Controllers
+{
+	[TestFixture]
+	public class SmapRejectorControllerFixture : BaseControllerTest
+	{
+		private SmapRejectorController _controller;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_controller = new SmapRejectorController();
+			SecurityContext.GetAdministrator = () => new Administrator { UserName = "TestAdmin" };
+			PrepareController(_controller);
+			ForTest.InitialzeAR();
+			RejectedEmail.DeleteAll();
+		}
+
+		[Test]
+		public void Show_method_should_return_rejected_messages_for_current_day()
+		{
+			var mail1 = new RejectedEmail { SmtpId = 64789, Comment = "нафиг", LogTime = DateTime.Now, From = "tech@analit.net", Subject = "test"};
+			mail1.SaveAndFlush();
+			var mail2 = new RejectedEmail { SmtpId = 65469, Comment = "еще нафиг", LogTime = DateTime.Now, From = "tech@analit.net", Subject = "test"};
+			mail2.SaveAndFlush();
+			var mail3 = new RejectedEmail { SmtpId = 64619, Comment = "снова нафиг", LogTime = DateTime.Now.AddDays(-1), From = "tech@analit.net", Subject = "test"};
+			mail3.SaveAndFlush();
+
+			_controller.Show();
+
+			var mails = (RejectedEmail[]) ControllerContext.PropertyBag["rejects"];
+			Assert.That(mails.Length, Is.EqualTo(2));
+			Assert.That(mails[0].Id, Is.EqualTo(mail1.Id));
+			Assert.That(mails[1].Id, Is.EqualTo(mail2.Id));
+			Assert.That(ControllerContext.PropertyBag["fromDate"], Is.EqualTo(DateTime.Today));
+			Assert.That(ControllerContext.PropertyBag["toDate"], Is.EqualTo(DateTime.Today));
+		}
+
+		[Test]
+		public void Search_should_find_rejects_with_given_text_in_from_and_subject_for_given_period()
+		{
+			var mail1 = new RejectedEmail { SmtpId = 64789, Comment = "нафиг", LogTime = new DateTime(2008, 10, 1, 12, 10, 00), From = "test@analit.net", Subject = "Самоучитель Windows Vista" };
+			mail1.SaveAndFlush();
+			var mail2 = new RejectedEmail { SmtpId = 65469, Comment = "нафиг", LogTime = new DateTime(2008, 10, 3, 15, 40, 00), From = "tech@analit.net", Subject = "Увеличение объемов продаж test" };
+			mail2.SaveAndFlush();
+			var mail3 = new RejectedEmail { SmtpId = 4613, Comment = "нафиг", LogTime = new DateTime(2008, 10, 2, 13, 1, 00), From = "tech@analit.net", Subject = "Увеличение объемов продаж" };
+			mail3.SaveAndFlush();
+			var mail4 = new RejectedEmail { SmtpId = 4634, Comment = "нафиг", LogTime = new DateTime(2008, 9, 30, 12, 10, 0), From = "test@analit.net", Subject = "Увеличение объемов продаж" };
+			mail4.SaveAndFlush();
+			var mail5 = new RejectedEmail { SmtpId = 4497, Comment = "нафиг", LogTime = new DateTime(2008, 10, 4, 8, 10, 0), From = "test@analit.net", Subject = "Увеличение объемов продаж" };
+			mail5.SaveAndFlush();
+
+			_controller.Search("test", new DateTime(2008, 10, 1), new DateTime(2008, 10, 3));
+
+			var mails = (RejectedEmail[])ControllerContext.PropertyBag["rejects"];
+			Assert.That(mails.Length, Is.EqualTo(2));
+			Assert.That(mails[0].Id, Is.EqualTo(mail1.Id));
+			Assert.That(mails[1].Id, Is.EqualTo(mail2.Id));
+			Assert.That(ControllerContext.PropertyBag["fromDate"], Is.EqualTo(new DateTime(2008, 10, 1)));
+			Assert.That(ControllerContext.PropertyBag["toDate"], Is.EqualTo(new DateTime(2008, 10, 3)));
+			Assert.That(ControllerContext.PropertyBag["searchText"], Is.EqualTo("test"));
+		}
+	}
+}
---
+++ src/AdminInterface.Test/Watin/SmapRejectorFixture.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using Microsoft.VisualStudio.WebHost;
+using NUnit.Framework;
+using WatiN.Core;
+
+namespace AdminInterface.Test.Watin
+{
+	[TestFixture]
+	public class SmapRejectorFixture
+	{
+		private Server _webServer;
+
+		[TestFixtureSetUp]
+		public void SetupFixture()
+		{
+			var port = int.Parse(ConfigurationManager.AppSettings["webPort"]);
+			var webDir = ConfigurationManager.AppSettings["webDirectory"];
+
+			_webServer = new Server(port, "/", Path.GetFullPath(webDir));
+			_webServer.Start();
+		}
+
+		[TestFixtureTearDown]
+		public void TearDownFixture()
+		{
+			_webServer.Stop();
+		}
+
+		private static string BuildTestUrl(string urlPart)
+		{
+			return String.Format("http://localhost:{0}/{1}",
+								 ConfigurationManager.AppSettings["webPort"],
+								 urlPart);
+		}
+
+		[Test]
+		public void Main_form_should_have_link_to_rejected_emails_where_should_be_emails_for_current_day()
+		{
+			using (var browser = new IE(BuildTestUrl("default.aspx")))
+			{
+
+			}
+		}
+	}
+}
