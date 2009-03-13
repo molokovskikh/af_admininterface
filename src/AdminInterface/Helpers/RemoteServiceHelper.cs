1487e1177ba2506e24467727c8ae3c344093c59d
kvasov <kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2009-03-13 09:51:14 +0000

    Исправил конфигурацию логирования

---
+++ src/AdminInterface/Helpers/RemoteServiceHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Principal;
+using System.ServiceModel;
+using ConsoleApplication11.ServiceReference2;
+using log4net;
+
+namespace AdminInterface.Helpers
+{
+	public class RemoteServiceHelper
+	{
+		private static readonly ILog _log = LogManager.GetLogger(typeof (RemoteServiceHelper));
+
+		public static void TryDoCall(Action<StatisticServiceClient> action)
+		{
+			using (WindowsIdentity.GetCurrent().Impersonate())
+			{
+				StatisticServiceClient client = null;
+				try
+				{
+					client = new StatisticServiceClient();
+					client.ClientCredentials.Windows.AllowedImpersonationLevel = TokenImpersonationLevel.Impersonation;
+					client.ClientCredentials.Windows.AllowNtlm = true;
+					action(client);
+					client.Close();
+				}
+				catch (Exception e)
+				{
+					if (client != null && client.State != CommunicationState.Closed)
+						client.Abort();
+
+					_log.Warn("Ошибка при обращении к сервису подготовки данных", e);
+				}
+			}
+		}
+	}
+}
