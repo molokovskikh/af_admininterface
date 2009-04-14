bea227ed969efff6ddc66417a7749133933b5195
kvasov <kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2009-04-14 09:29:30 +0000

    Исправил обработку тразакций

--- src/AdminInterface/default.aspx.cs
+++ src/AdminInterface/default.aspx.cs
@@ -4,19 +4,17 @@ using System.Web.UI;
 using AdminInterface.Helpers;
 using AdminInterface.Models.Security;
 using AdminInterface.Security;
+using Common.MySql;
 using MySql.Data.MySqlClient;
 
 namespace AddUser
 {
 	partial class _default : Page
 	{
-		private readonly MySqlConnection _connection = new MySqlConnection();
-
 		protected void Page_Load(object sender, EventArgs e)
 		{
 			SecurityContext.CheckIsUserAuthorized();
 
-			_connection.ConnectionString = Literals.GetConnectionString();
 			var expirationDate = ADHelper.GetPasswordExpirationDate(SecurityContext.Administrator.UserName);
 			if (expirationDate >= DateTime.Now || SecurityContext.Administrator.UserName == "michail")
 			{
@@ -30,7 +28,9 @@ namespace AddUser
 					ToCalendar.SelectedDates.Add(DateTime.Now);
 					FromCalendar.SelectedDates.Add(DateTime.Now);
 					var data = new DataSet();
-					var adapter = new MySqlDataAdapter(@"
+					With.Connection(
+						c => {
+							var adapter = new MySqlDataAdapter(@"
 SELECT (SELECT bit_or(RegionCode)
 FROM AccessRight.RegionalAdmins ra
   INNER JOIN Farm.Regions r on r.RegionCode & ra.RegionMask > 0
@@ -42,10 +42,11 @@ FROM
 FROM AccessRight.RegionalAdmins ra
   INNER JOIN Farm.Regions r on r.RegionCode & ra.RegionMask > 0
 WHERE UserName = ?UserName ORDER BY Region) tmp;
-;
-", _connection);
-					adapter.SelectCommand.Parameters.AddWithValue("?UserName", SecurityContext.Administrator.UserName);
-					adapter.Fill(data);
+;",c);
+							adapter.SelectCommand.Parameters.AddWithValue("?UserName", SecurityContext.Administrator.UserName);
+							adapter.Fill(data);
+						});
+					
 					RegionList.DataTextField = "Region";
 					RegionList.DataValueField = "RegionCode";
 					RegionList.DataSource = data;
@@ -76,30 +77,25 @@ WHERE UserName = ?UserName ORDER BY Region) tmp;
 
 		public void GetStatistics(ulong regionMask, DateTime fromDate, DateTime toDate)
 		{
-			try
-			{
 				var urlTemplate = String.Format("BeginDate={0}&EndDate={1}&RegionMask={2}", fromDate, toDate.AddDays(1), regionMask);
 				CUHL.NavigateUrl = String.Format("viewcl.aspx?id={0}&{1}", (int)StatisticsType.UpdateCumulative, urlTemplate);
 				ErrUpHL.NavigateUrl = String.Format("viewcl.aspx?id={0}&{1}", (int)StatisticsType.UpdateError, urlTemplate);
 				ADHL.NavigateUrl = String.Format("viewcl.aspx?id={0}&{1}", (int)StatisticsType.UpdateBan, urlTemplate);
 				ConfHL.NavigateUrl = String.Format("viewcl.aspx?id={0}&{1}", (int)StatisticsType.UpdateNormal, urlTemplate);
 
-			    _connection.Open();
-				var transaction = _connection.BeginTransaction(IsolationLevel.ReadCommitted);
-				var adapter = new MySqlDataAdapter("GetShowStat", _connection)
-				                  {
-				                      SelectCommand =
-				                          {
-				                              Transaction = transaction,
-				                              CommandType = CommandType.StoredProcedure
-				                          }
-				                  };
-			    adapter.SelectCommand.Parameters.AddWithValue("?StartDateParam", fromDate);
-				adapter.SelectCommand.Parameters.AddWithValue("?EndDateParam", toDate.AddDays(1));
-				adapter.SelectCommand.Parameters.AddWithValue("?RegionMaskParam", regionMask);
-				adapter.SelectCommand.Parameters.AddWithValue("?UserNameParam", SecurityContext.Administrator.UserName);
 				var data = new DataSet();
-				adapter.Fill(data);
+				With.Connection(
+					c => {
+						var adapter = new MySqlDataAdapter("GetShowStat", c)
+						{
+							SelectCommand = { CommandType = CommandType.StoredProcedure }
+						};
+						adapter.SelectCommand.Parameters.AddWithValue("?StartDateParam", fromDate);
+						adapter.SelectCommand.Parameters.AddWithValue("?EndDateParam", toDate.AddDays(1));
+						adapter.SelectCommand.Parameters.AddWithValue("?RegionMaskParam", regionMask);
+						adapter.SelectCommand.Parameters.AddWithValue("?UserNameParam", SecurityContext.Administrator.UserName);
+						adapter.Fill(data);						
+					});
 				//Заказы
 				//Количество принятых заказов
 				OPLB.Text = data.Tables[0].Rows[0]["OrdersCount"].ToString();
@@ -197,12 +193,6 @@ WHERE UserName = ?UserName ORDER BY Region) tmp;
 					    ConfHL.Enabled = true;
 				}
 				catch (Exception) { }
-				transaction.Commit();
-			}
-			finally
-			{
-				_connection.Close();
-			}
 		}
 	}
 }
