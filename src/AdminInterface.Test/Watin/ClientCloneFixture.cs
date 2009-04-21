088f5a58a81ee8a9d21cf9e616421a17f2bbf0ad
kvasov <kvasov@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2009-04-21 10:43:37 +0000

    Тест для клонирования

---
+++ src/AdminInterface.Test/Watin/ClientCloneFixture.cs
@@ -0,0 +1,24 @@
+﻿using AdminInterface.Test.ForTesting;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using WatiN.Core;
+
+namespace AdminInterface.Test.Watin
+{
+	[TestFixture]
+	public class ClientCloneFixture : WatinFixture
+	{
+		[Test]
+		public void Try_to_clone_client()
+		{
+			using (var browser = new IE(BuildTestUrl("CopySynonym.aspx")))
+			{
+				browser.TextField(Find.ById("ctl00_MainContentPlaceHolder_FromTB")).TypeText("ТестерК");
+				browser.TextField(Find.ById("ctl00_MainContentPlaceHolder_ToTB")).TypeText("ТестерК2");
+				browser.Button(Find.ByValue("Найти")).Click();
+				browser.Button(Find.ByValue("Присвоить")).Click();
+				Assert.That(browser.Text, Text.Contains("Присвоение успешно завершен"));
+			}
+		}
+	}
+}
