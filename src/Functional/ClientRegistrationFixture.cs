6282d5154adc8b4a0a7d03496492d8d5403ccf7d
belenko <belenko@01136f43-ed8a-6e48-b5f0-2318192a8bb8>
2009-12-08 11:44:40 +0000

    Добавил тест для проверки билинга при регистрации

---
+++ src/Functional/ClientRegistrationFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using AdminInterface.Test.ForTesting;
+using WatiN.Core;
+
+namespace Functional
+{
+	[TestFixture]
+	public class ClientRegistrationFixture : WatinFixture
+	{
+		[Test]
+		public void SetExistsBilingTest()
+		{
+			using(var browser = Open("main/index"))
+			{
+				browser.Link(Find.ByText("Регистрация клиентов")).Click();
+				browser.CheckBox("PayerPresentCB").Checked = true;
+				browser.TextField("PayerFTB").TypeText("офис");
+				browser.Button("FindPayerB").Click();
+
+				Assert.AreEqual("Регистрация пользователей", browser.Title);
+			}
+		}
+	}
+}
