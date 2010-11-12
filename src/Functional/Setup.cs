2b70fff8f20b71c17f0ad8db2335b3257aea26a9
Квасов Роман <r.kvasov@analit.net>
2010-11-12 18:42:35 +0300

    исправил тесты

--- src/Functional/Setup.cs
+++ src/Functional/Setup.cs
@@ -1,9 +1,18 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.IO;
+using AdminInterface.Models.Security;
 using AdminInterface.Test.ForTesting;
+using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Core;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
 using Microsoft.VisualStudio.WebHost;
 using NUnit.Framework;
 using WatiN.Core;
+using SecurityContext = AdminInterface.Security.SecurityContext;
 
 namespace Functional
 {
@@ -16,6 +25,7 @@ namespace Functional
 		public void SetupFixture()
 		{
 			ForTest.InitialzeAR();
+			SecurityContext.GetAdministrator = () => new Administrator{UserName = "test"};
 
 			var port = int.Parse(ConfigurationManager.AppSettings["webPort"]);
 			var webDir = ConfigurationManager.AppSettings["webDirectory"];
@@ -24,6 +34,26 @@ namespace Functional
 			_webServer.Start();
 			Settings.Instance.AutoMoveMousePointerToTopLeft = false;
 			Settings.Instance.MakeNewIeInstanceVisible = false;
+
+			InitLogger(/*"NHibernate"*/);
+		}
+
+		private void InitLogger()
+		{
+/*			var repository = LogManager.GetRepository();
+			var hierarchy = ((Hierarchy)repository);
+			hierarchy.Root.Level = Level.Debug;
+
+			PatternLayout layout = new PatternLayout();
+			layout.ConversionPattern = PatternLayout.DetailConversionPattern;
+			layout.ActivateOptions();
+
+			// Create the appender
+			ConsoleAppender appender = new ConsoleAppender();
+			appender.Layout = layout;
+			appender.ActivateOptions();
+			hierarchy.Root.AddAppender(new ConsoleAppender());
+			hierarchy.Configured = true;*/
 		}
 
 		[TearDown]
