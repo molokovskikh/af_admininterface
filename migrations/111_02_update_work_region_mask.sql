a1202c73acb44bd379058c90f001252c2212b5bb
Квасов Роман <r.kvasov@analit.net>
2011-04-21 17:46:35 +0400

    исправил маски для пользователей поставщиков

--- migrations/111_02_update_work_region_mask.sql
+++ migrations/111_02_update_work_region_mask.sql
@@ -1,4 +1,4 @@
 update Future.Users u
-join Usersettings.osuseraccessright ouar
+join Usersettings.osuseraccessright ouar on u.Login = ouar.OsUserName
 set u.OrderRegionMask = ouar.RegionMask, u.WorkRegionMask = ouar.RegionMask
 where u.OrderRegionMask is null
