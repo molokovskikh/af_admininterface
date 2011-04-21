ada6bed36b0ddae0c28216beabaa0440bfcfeef9
Квасов Роман <r.kvasov@analit.net>
2011-04-21 15:54:27 +0400

    добавил тест для поиска заказов по пользователю

---
+++ src/Integration/Models/OrderFilterFixture.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using AdminInterface.Controllers;
+using Integration.ForTesting;
+using NUnit.Framework;
+
+namespace Integration.Models
+{
+	[TestFixture]
+	public class OrderFilterFixture
+	{
+		[Test]
+		public void Find_order_for_user()
+		{
+			var client = DataMother.CreateTestClientWithUser();
+			var user = client.Users.First();
+			OrderLog.Load(new OrderFilter {
+				User = user,
+				Client = client
+			});
+		}
+	}
+}
