87b917ece0dcbfb1ba92bc1f2b8feba390464314
Квасов Роман <r.kvasov@analit.net>
2011-04-21 14:07:49 +0400

    исправил миграцию

--- migrations/84_06_move_suppliers.sql
+++ migrations/84_06_move_suppliers.sql
@@ -1,10 +1,10 @@
 insert into Future.Suppliers(Id, Name, FullName, Disabled, RegionMask, 
 	Registrant, RegistrationDate, HomeRegion, ContactGroupOwnerId)
-select FirmCode, ShortName, FullName, 0, MaskRegion, Registrant,
+select FirmCode, ShortName, FullName, FirmStatus = 0, MaskRegion, Registrant,
 	RegistrationDate, RegionCode, ContactGroupOwnerId
 from Usersettings.ClientsData
-where FirmStatus = 1 and FirmType = 0 and FirmSegment = 0;
+where FirmType = 0 and FirmSegment = 0;
 
-insert into Future.Services(Id, Name, HomeRegion)
-select Id, Name, HomeRegion
+insert into Future.Services(Id, Name, HomeRegion, Disabled)
+select Id, Name, HomeRegion, Disabled
 from Future.Suppliers;
