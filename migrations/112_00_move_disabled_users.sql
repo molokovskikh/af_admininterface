ba070f4a4a3ae46c3df8ad0e2a454fcaf7a5b59f
Квасов Роман <r.kvasov@analit.net>
2011-04-22 15:24:14 +0400

    создал недостающих пользователей

---
+++ migrations/112_00_move_disabled_users.sql
@@ -0,0 +1,89 @@
+insert into Future.Users(Id, Enabled, Login, RootService, PayerId, WorkRegionMask, OrderRegionMask)
+select ouar.RowId, cd.FirmStatus = 1, ouar.OsUserName, cd.FirmCode, cd.BillingCode, ouar.RegionMask, ouar.RegionMask
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0;
+
+insert into usersettings.AssignedPermissions(UserId, PermissionId)
+select ouar.RowId,
+(select Id from Usersettings.UserPermissions p where p.Shortcut = 'VendorAllowContactInfo')
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0 and ouar.VendorAllowContactInfo = 1;
+
+insert into usersettings.AssignedPermissions(UserId, PermissionId)
+select ouar.RowId,
+(select Id from Usersettings.UserPermissions p where p.Shortcut = 'VendorAllowChangePassword')
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0 and ouar.VendorAllowChangePassword = 1;
+
+insert into usersettings.AssignedPermissions(UserId, PermissionId)
+select ouar.RowId,
+(select Id from Usersettings.UserPermissions p where p.Shortcut = 'VendorAllowOrdersManage')
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0 and ouar.VendorAllowOrdersManage = 1;
+
+insert into usersettings.AssignedPermissions(UserId, PermissionId)
+select ouar.RowId,
+(select Id from Usersettings.UserPermissions p where p.Shortcut = 'VendorAllowReports')
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0 and ouar.VendorAllowReports = 1;
+
+insert into usersettings.AssignedPermissions(UserId, PermissionId)
+select ouar.RowId,
+(select Id from Usersettings.UserPermissions p where p.Shortcut = 'VendorAllowConManage')
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0 and ouar.VendorAllowConManage = 1;
+
+insert into usersettings.AssignedPermissions(UserId, PermissionId)
+select ouar.RowId,
+(select Id from Usersettings.UserPermissions p where p.Shortcut = 'VendorAllowClients')
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0 and ouar.VendorAllowClients = 1;
+
+insert into usersettings.AssignedPermissions(UserId, PermissionId)
+select ouar.RowId,
+(select Id from Usersettings.UserPermissions p where p.Shortcut = 'VendorAllowPricesSet')
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0 and ouar.VendorAllowPricesSet = 1;
+
+insert into usersettings.AssignedPermissions(UserId, PermissionId)
+select ouar.RowId,
+(select Id from Usersettings.UserPermissions p where p.Shortcut = 'VendorAllowPricesSources')
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0 and ouar.VendorAllowPricesSources = 1;
+
+insert into usersettings.AssignedPermissions(UserId, PermissionId)
+select ouar.RowId,
+(select Id from Usersettings.UserPermissions p where p.Shortcut = 'VendorAllowDocumentSettings')
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0 and ouar.VendorAllowDocumentSettings = 1;
+
+insert into usersettings.AssignedPermissions(UserId, PermissionId)
+select ouar.RowId,
+(select Id from Usersettings.UserPermissions p where p.Shortcut = 'VendorAllowDocumentLogs')
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0 and ouar.VendorAllowDocumentLogs = 1;
+
+insert into usersettings.AssignedPermissions(UserId, PermissionId)
+select ouar.RowId,
+(select Id from Usersettings.UserPermissions p where p.Shortcut = 'VendorAllowSourceOrders')
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0 and ouar.VendorAllowSourceOrders = 1;
+
+insert into usersettings.AssignedPermissions(UserId, PermissionId)
+select ouar.RowId,
+(select Id from Usersettings.UserPermissions p where p.Shortcut = 'VendorAllowPromotions')
+from Usersettings.OsUserAccessRight ouar
+join Usersettings.ClientsData cd on cd.FirmCode = ouar.ClientCode
+where cd.FirmStatus = 0 and cd.FirmSegment = 0 and cd.FirmType = 0 and ouar.VendorAllowPromotions = 1;
