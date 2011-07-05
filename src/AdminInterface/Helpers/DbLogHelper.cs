4f708482391204144eb6c5863088aca4539b66df
Золотарев Александр <a.zolotarev@analit.net>
2011-07-05 17:50:24 +0400

    Перенес логирование тригерами пользователей в common.web.ui

--- src/AdminInterface/Helpers/DbLogHelper.cs
+++ src/AdminInterface/Helpers/DbLogHelper.cs
@@ -6,7 +6,7 @@ using NHibernate;
 
 namespace AdminInterface.Helpers
 {
-	public class DbLogHelper
+	public class DbLogHelper : DbLogHelperBase
 	{
 		public static void SetupParametersForTriggerLogging()
 		{
@@ -15,38 +15,6 @@ namespace AdminInterface.Helpers
 					InHost = SecurityContext.Administrator.Host
 				},
 				session));
-		}
-
-		public static void SetupParametersForTriggerLogging(string user, string host)
-		{
-			ArHelper.WithSession(session => SetupParametersForTriggerLogging(new { InUser = user, InHost = host }, session));
-		}
-
-		public static void SetupParametersForTriggerLogging(object parameters)
-		{
-			ArHelper.WithSession(session => SetupParametersForTriggerLogging(parameters, session));
-		}
-
-		private static void SetupParametersForTriggerLogging(object parameters, ISession session)
-		{
-			using (var command = session.Connection.CreateCommand())
-			{
-				foreach (var property in parameters.GetType().GetProperties(BindingFlags.GetProperty
-																					 | BindingFlags.Public
-																					 | BindingFlags.Instance))
-				{
-					var value = property.GetValue(parameters, null);
-					command.CommandText += String.Format(" SET @{0} = ?{0}; ", property.Name);
-					var parameter = command.CreateParameter();
-					parameter.Value = value;
-					parameter.ParameterName = "?" + property.Name;
-					command.Parameters.Add(parameter);
-				}
-				if (command.Parameters.Count == 0)
-					return;
-
-				command.ExecuteNonQuery();
-			}
 		}
 	}
 }
