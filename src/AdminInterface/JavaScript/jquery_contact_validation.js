a8a70bd1cb404302f149a6b699b8a6a524b86577
Квасов Роман <r.kvasov@analit.net>
2011-08-29 10:39:54 +0400

    удалил устаревший код для валидации

--- src/AdminInterface/JavaScript/jquery_contact_validation.js
+++ src/AdminInterface/JavaScript/jquery_contact_validation.js
@@ -3,27 +3,31 @@
 // и при загрузке документа вызвать метод validate у формы
 jQuery(function() {
 	jQuery.validator.addMethod("phone", function(value, element) {
-            if (value.toString().length > 0) {
-                res = /^(\d{3,4})-(\d{6,7})(\*\d{3})?$/.test(value)
-                return res;
-            }
-            return true;
-        }, "Некорректный телефонный номер");
+			if (value.toString().length > 0) {
+				res = /^(\d{3,4})-(\d{6,7})(\*\d{3})?$/.test(value)
+				return res;
+			}
+			return true;
+		}, "Некорректный телефонный номер");
 		
 	// Валидатор для внутреннего номера
 	jQuery.validator.addMethod("InternalPhone", function(value, element) {
-            if (value.toString().length > 0) {
-                res = /^(\d{3})$/.test(value)
-                return res;
-            }
-            return true;
-        }, "Некорректный телефонный номер");		
+			if (value.toString().length > 0) {
+				res = /^(\d{3})$/.test(value)
+				return res;
+			}
+			return true;
+		}, "Некорректный телефонный номер");
 
-        jQuery.validator.addMethod("email", function(value, element) {
-            if (value.toString().length > 0) {
-                res = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/.test(value)
-                return res;
-            }
-            return true;
-        }, "Некорректный адрес электронной почты");  
+	$.validator.addMethod("mobile-phone", function (v) {
+			return /^\d{10}$/.test(v);
+		}, "Телефон должен быть десятизначный");
+
+	jQuery.validator.addMethod("email", function(value, element) {
+			if (value.toString().length > 0) {
+				res = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/.test(value)
+				return res;
+			}
+			return true;
+		}, "Некорректный адрес электронной почты");
 });
