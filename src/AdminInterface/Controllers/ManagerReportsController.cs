21a442eda0998ac2c21402e745b0e477ff22e2cc
Золотарев Александр <a.zolotarev@analit.net>
2012-01-11 16:44:28 +0400

    Предрелизная версия без тестов

--- src/AdminInterface/Controllers/ManagerReportsController.cs
+++ src/AdminInterface/Controllers/ManagerReportsController.cs
@@ -8,16 +8,53 @@ using Castle.MonoRail.ActiveRecordSupport;
 using Castle.MonoRail.Framework;
 using Common.Web.Ui.Helpers;
 using Common.Web.Ui.Models;
+using Common.Web.Ui.MonoRailExtentions;
+using Common.Web.Ui.NHibernateExtentions;
 
 namespace AdminInterface.Controllers
 {
-	public class RequestFilter : Sortable, IPaginable
+	public enum RegistrationFinderType
 	{
-		public DateTime? startDate { get; set; }
-		public DateTime? endDate { get; set; }
+		Users,
+		Adresses
+	}
+
+	public class RegistrationInformation
+	{
+		public uint Id { get; set; }
+		public string Name { get; set; }
+		public DateTime RegistrationDate { get; set; }
+		public bool AdressEnabled { get; set; }
+		public bool ClientEnabled { get; set; }
+		public bool UserEnabled { get; set; }
+		public bool ServiceDisabled { get; set; }
+	}
+
+	public class RegistrationFinderTypeProperties : ICompisite
+	{
+		public RegistrationFinderType Value { get; set; }
+
+		public bool IsUsers()
+		{
+			return Value == RegistrationFinderType.Users;
+		}
+
+		public bool IsAdresses()
+		{
+			return Value == RegistrationFinderType.Adresses;
+		}
+
+		public IEnumerable<Tuple<string, object>> Elements()
+		{
+			return new[] {new Tuple<string, object>("Value", Value)};
+		}
+	}
+
+	public class UserFinderFilter : Sortable, IPaginable
+	{
 		public long region { get; set; }
-		//public string Direction { get; set; }
-		//public string SortBy { get; set; }
+		public RegistrationFinderTypeProperties finderType { get; set; }
+		public DatePeriod Period { get; set; }
 
 		private int _lastRowsCount;
 
@@ -28,7 +65,7 @@ namespace AdminInterface.Controllers
 
 		public int PageSize
 		{
-			get { return 20; }
+			get { return 30; }
 		}
 
 		public int CurrentPage { get; set; }
@@ -43,12 +80,12 @@ namespace AdminInterface.Controllers
 		public Dictionary<string, object> GetParameters()
 		{
 			return new Dictionary<string, object> {
-				{"filter.startDate", startDate},
-				{"filter.endDate", endDate},
+				{"filter.Period.Begin", Period.Begin},
+				{"filter.Period.End", Period.End},
 				{"filter.region", region},
+				{"filter.finderType.Value", finderType.Value},
 				{"filter.Direction", SortDirection},
 				{"filter.SortBy", SortBy}
-
 			};
 		}
 
@@ -63,42 +100,65 @@ namespace AdminInterface.Controllers
 			return ToUrlQuery();
 		}
 
-		public IList<User> Find()
+		private string GetFieldToSort(string originalField)
 		{
-			var users = ArHelper.WithSession(s => {
-				var newSql =
-					string.Format(
+			if (finderType.Value == RegistrationFinderType.Adresses) {
+				if (originalField == "Name")
+					return "Address";
+			}
+			return originalField;
+		}
+
+		public IList<RegistrationInformation> Find()
+		{
+			return ArHelper.WithSession<IList<RegistrationInformation>>(s => {
+				var newSql = string.Empty;
+				if (finderType.Value == RegistrationFinderType.Users)
+					newSql = string.Format(
 						@"
-SELECT u.* FROM future.users u
+SELECT u.Id, u.Name, u.RegistrationDate, u.Enabled as UserEnabled, s.Disabled as ServiceDisabled
+FROM future.users u
 join future.services s on s.id = u.rootservice
 where
 s.HomeRegion & :region > 0
 and RegistrationDate >= :startDate
 and RegistrationDate <= :endDate
+and u.PayerId <> 921
 ORDER BY {2}
-limit {0}, {1}",
-						CurrentPage*PageSize, PageSize, (string.IsNullOrEmpty(SortBy) ? "u.Id" : "u." + SortBy) + " " + SortDirection);
-				var result =
-					s.CreateSQLQuery(newSql)
-						.AddEntity(typeof (User))
+limit {0}, {1}", CurrentPage*PageSize, PageSize, (string.IsNullOrEmpty(SortBy) ? "u.Id" : "u." + GetFieldToSort(SortBy)) + " " + SortDirection);
+
+				if (finderType.Value == RegistrationFinderType.Adresses)
+					newSql = string.Format(
+						@"
+SELECT a.Id, a.Address as Name, a.RegistrationDate, a.Enabled as AdressEnabled, c.Status as ClientEnabled
+FROM future.addresses a
+join future.Clients c on a.ClientId = c.id
+where
+c.RegionCode & :region > 0
+and a.RegistrationDate >= :startDate
+and a.RegistrationDate <= :endDate
+ORDER BY {2}
+limit {0}, {1}", CurrentPage*PageSize, PageSize, (string.IsNullOrEmpty(SortBy) ? "a.Id" : "a." + GetFieldToSort(SortBy)) + " " + SortDirection);
+
+				var result = s.CreateSQLQuery(newSql)
 						.SetParameter("region", region)
-						.SetParameter("startDate", startDate)
-						.SetParameter("endDate", endDate)
-						.List<User>();
+						.SetParameter("startDate", Period.Begin)
+						.SetParameter("endDate", Period.End)
+						.ToList<RegistrationInformation>();
 
 				newSql = newSql.Remove(newSql.IndexOf("limit"));
 				newSql = string.Format("select count(*) from ({0}) as t1;", newSql);
 				var countQuery = s.CreateSQLQuery(newSql);
 				countQuery.SetParameter("region", region)
-					.SetParameter("startDate", startDate)
-					.SetParameter("endDate", endDate);
+					.SetParameter("startDate", Period.Begin)
+					.SetParameter("endDate", Period.End);
 				_lastRowsCount = Convert.ToInt32(countQuery.UniqueResult());
 
 				return result;
 			});
-			return users;
 		}
 	}
+
 
 	[
 		Helper(typeof(PaginatorHelper), "paginator"),
@@ -106,22 +166,19 @@ limit {0}, {1}",
 	]
 	public class ManagerReportsController: ARSmartDispatcherController
 	{
-		public void UsersAndAdresses([DataBind("filter")]RequestFilter filter)
+		public void UsersAndAdresses([DataBind("filter")]UserFinderFilter userFilter)
 		{
 			PropertyBag["Regions"] = RegionHelper.GetAllRegions();
-			PropertyBag["chRegion"] = filter.region;
+			PropertyBag["chRegion"] = userFilter.region;
 
-			if (filter.startDate == null)
-				filter.startDate = DateTime.Now.AddDays(-1);
-			if (filter.endDate == null)
-				filter.endDate = DateTime.Now;
+			if (userFilter.Period == null)
+				userFilter.Period = new DatePeriod(DateTime.Now.AddDays(-1), DateTime.Now);
+			if (userFilter.finderType == null)
+				userFilter.finderType = new RegistrationFinderTypeProperties {Value = RegistrationFinderType.Users};
 
-			PropertyBag["startDate"] = filter.startDate.Value.ToString("yyyy-MM-dd");
-			PropertyBag["endDate"] = filter.endDate.Value.ToString("yyyy-MM-dd");
+			PropertyBag["filter"] = userFilter;
 
-			PropertyBag["filter"] = filter;
-
-			PropertyBag["Users"] = filter.Find();
+			PropertyBag["Users"] = userFilter.Find();
 		}
 	}
 }
