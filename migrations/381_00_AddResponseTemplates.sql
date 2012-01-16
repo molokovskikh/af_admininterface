0b0c3c53fdd4b9899fbd1f3686bb8e6572059fb7
Morozov Sergey <morozov@analit.net>
2012-01-16 13:48:17 +0400

    Добавил значения по умолчанию для шаблонов.

---
+++ migrations/381_00_AddResponseTemplates.sql
@@ -0,0 +1,8 @@
+﻿update UserSettings.Defaults 
+set
+  ResponseBodyMiniMailOnUnknownProvider = 'Предполагаемая дата запуска функционала 20.01.12.',
+  ResponseBodyMiniMailOnEmptyRecipients = 'Предполагаемая дата запуска функционала 20.01.12.',
+  ResponseBodyMiniMailOnMaxAttachment = 'Предполагаемая дата запуска функционала 20.01.12.',
+  ResponseBodyMiniMailOnAllowedExtensions = 'Предполагаемая дата запуска функционала 20.01.12.'
+where
+  Id = 1;
