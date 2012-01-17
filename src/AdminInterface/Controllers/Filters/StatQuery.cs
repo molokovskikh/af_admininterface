76a9f14bb6596b8237601533e0d9ab1d9887fc81
Квасов Роман <r.kvasov@analit.net>
2012-01-17 14:31:35 +0400

    Новшество #7925. Блоки "Накладные", "Сертификаты", "Минипочта" отображать в виде ссылок.

---
+++ src/AdminInterface/Controllers/Filters/StatQuery.cs
@@ -0,0 +1,182 @@
+using System;
+using System.ComponentModel;
+using System.Data;
+using AdminInterface.Security;
+using Common.MySql;
+using Common.Tools;
+using MySql.Data.MySqlClient;
+
+namespace AdminInterface.Controllers.Filters
+{
+	public class StatQuery
+	{
+		[Description("Полностью")]
+		public bool Full { get; set; }
+
+		public DataSet Load(ulong regionMask, DateTime fromDate, DateTime toDate)
+		{
+			var data = new DataSet();
+
+			var additionalSql =
+				@"SELECT cast(concat(count(dlogs.RowId), '(', count(DISTINCT dlogs.ClientCode), ')') as CHAR) as CountDownloadedWaybills,
+	   max(dlogs.LogTime) as LastDownloadedWaybillDate
+FROM logs.document_logs dlogs
+join usersettings.retclientsset rcs on rcs.ClientCode = dlogs.ClientCode
+WHERE (dlogs.LogTime >= ?StartDateParam AND dlogs.LogTime <= ?EndDateParam) AND dlogs.DocumentType = 1 and rcs.ParseWaybills = 1;
+
+SELECT cast(concat(count(dheaders.Id), '(', count(DISTINCT dheaders.ClientCode), ')') as CHAR) as CountParsedWaybills,
+	   max(dheaders.WriteTime) as LastParsedWaybillDate
+FROM documents.documentheaders dheaders
+WHERE (dheaders.WriteTime >= ?StartDateParam AND dheaders.WriteTime <= ?EndDateParam) AND dheaders.DocumentType = 1;
+
+select ifnull(sum(if(db.ProductId is not null, 1, 0)), 0) as DocumentProductIdentifiedCount,
+	ifnull(sum(if(db.ProducerId is not null, 1, 0)), 0) as DocumentProducerIdentifiedCount,
+	count(*) as DocumentLineCount
+from documents.documentheaders d
+	join documents.documentbodies db on db.DocumentId = d.Id
+where (d.WriteTime >= ?StartDateParam AND d.WriteTime <= ?EndDateParam);
+
+select count(*) as TotalCertificates,
+sum(if(l.Filename is null, 1, 0)) as TotalNotSendCertificates,
+sum(if(l.Filename is not null, 1, 0)) as TotalSendCertificates
+from Logs.CertificateRequestLogs l
+	join Logs.AnalitFUpdates u on u.UpdateId = l.UpdateId
+		join Future.Users fu on fu.Id = u.UserId
+			join Future.Clients c on c.Id = fu.ClientId
+where (u.RequestTime >= ?StartDateParam AND u.RequestTime <= ?EndDateParam)
+and c.MaskRegion & ?RegionMaskParam > 0
+;
+
+select max(u.RequestTime) as LastCertificateRequest
+from Logs.CertificateRequestLogs l
+	join Logs.AnalitFUpdates u on u.UpdateId = l.UpdateId
+		join Future.Users fu on fu.Id = u.UserId
+			join Future.Clients c on c.Id = fu.ClientId
+where c.MaskRegion & ?RegionMaskParam > 0
+;
+
+select count(*) TotalMails,
+max(m1.Size) MaxMailSize,
+avg(m1.Size) AvgMailSize
+from (
+		select m.Id
+		from Documents.Mails m
+			join Logs.MailSendLogs l on l.MailId = m.Id
+				join Future.Users u on u.Id = l.UserId
+					join Future.Clients c on c.Id = u.ClientId
+		where m.LogTime >= ?StartDateParam AND m.LogTime <= ?EndDateParam
+			and c.MaskRegion & ?RegionMaskParam > 0
+		group by m.Id
+	) as bm
+	join Documents.Mails m1 on m1.Id = bm.Id
+;
+
+select count(*) as TotalMailRecipients
+from Documents.Mails m
+	join Logs.MailSendLogs l on l.MailId = m.Id
+		join Future.Users u on u.Id = l.UserId
+			join Future.Clients c on c.Id = u.ClientId
+where m.LogTime >= ?StartDateParam AND m.LogTime <= ?EndDateParam
+	and c.MaskRegion & ?RegionMaskParam > 0
+;
+
+select max(m.LogTime) as LastMailSend
+from Documents.Mails m
+	join Logs.MailSendLogs l on l.MailId = m.Id
+		join Future.Users u on u.Id = l.UserId
+			join Future.Clients c on c.Id = u.ClientId
+where c.MaskRegion & ?RegionMaskParam > 0
+;";
+				var mainSql = @"
+SELECT max(UpdateDate) MaxUpdateTime
+FROM future.Clients cd
+	join future.Users u on u.ClientId = cd.Id
+		JOIN usersettings.UserUpdateInfo uui on uui.UserId = u.Id
+WHERE   cd.RegionCode & ?RegionMaskParam > 0
+		AND uui.UncommitedUpdateDate >= ?StartDateParam AND uui.UncommitedUpdateDate <= ?EndDateParam;
+
+SELECT cast(concat(count(if(resultid=2, PriceItemId, null)), '(', count(DISTINCT if(resultid=2, PriceItemId, null)), ')') as CHAR) as FormCount,
+	   cast(ifnull(max(if(resultid=2, logtime, null)), '2000-01-01') as CHAR) as LastForm
+FROM logs.formlogs
+WHERE logtime >= ?StartDateParam AND logtime <= ?EndDateParam;
+
+SELECT cast(concat(count(if(resultcode=2, PriceItemId, null)), '(', count(DISTINCT if(resultcode=2, PriceItemId, null)), ')') as CHAR) as DownCount,
+	   cast(ifnull(max(if(resultcode=2, logtime, null)), '2000-01-01') as CHAR) as LastDown
+FROM logs.downlogs
+WHERE logtime >= ?StartDateParam AND logtime <= ?EndDateParam;
+
+SELECT count(DISTINCT oh.clientcode) as UniqClientOrders,
+	   max(WriteTime) as MaxOrderTime
+		{1}
+FROM (orders.ordershead oh,
+	 orders.orderslist ol, 
+	 future.Clients cd, 
+	 usersettings.retclientsset rcs)
+	join Future.Users u on u.Id = oh.UserId
+WHERE oh.rowid = orderid
+	AND cd.Id = oh.clientcode
+	AND u.PayerId <> 921
+	AND rcs.clientcode = oh.clientcode
+	AND cd.Segment = 0
+	AND rcs.serviceclient = 0 
+	AND oh.regioncode & ?RegionMaskParam   > 0
+	AND oh.Deleted = 0
+	AND oh.Submited = 1
+	AND WriteTime >= ?StartDateParam AND WriteTime <= ?EndDateParam;
+
+select count(oh.RowId) as NonProcOrdersCount
+from orders.ordershead oh
+where	oh.processed = 0
+		and oh.submited = 1
+		and oh.deleted = 0;
+
+SELECT sum(if(afu.UpdateType = 6, 1, 0)) as UpdatesErr,
+	   cast(concat(Sum(afu.UpdateType IN (5)) ,'(' ,count(DISTINCT if(afu.UpdateType  IN (5), u.Id, null)) ,')') as CHAR) UpdatesAD,
+	   cast(concat(sum(afu.UpdateType = 2) ,'(' ,count(DISTINCT if(afu.UpdateType = 2, u.Id, null)) ,')') as CHAR) CumulativeUpdates,
+	   cast(concat(sum(afu.UpdateType = 1) ,'(' ,count(DISTINCT if(afu.UpdateType = 1, u.Id, null)) ,')') as CHAR) Updates
+		{0}
+FROM Future.Clients cd
+	join Future.Users u on u.ClientId = cd.Id
+	join logs.AnalitFUpdates afu on afu.UserId = u.Id
+WHERE cd.maskregion & ?RegionMaskParam > 0
+	  AND afu.RequestTime >= ?StartDateParam AND afu.RequestTime <= ?EndDateParam;
+
+
+";
+			var additionalUpdateStatColumns = new[] {
+				"ifnull(sum(if(afu.UpdateType in (1,2), resultsize, 0)), 0) as DownloadDataSize",
+				"ifnull(sum(if(afu.UpdateType in (8), resultsize, 0)), 0) as DownloadDocumentSize"
+			};
+
+			var additionalOrderColumns = new[] {
+				"count(DISTINCT oh.userid) as UniqUserOrders",
+				"count(DISTINCT oh.rowid) as TotalOrders",
+				"sum(ol.cost * ol.quantity) as OrderSum"
+			};
+			string sql;
+
+			if (Full)
+			{
+				sql = String.Format(mainSql,
+					"," + additionalUpdateStatColumns.Implode(","),
+					"," + additionalOrderColumns.Implode(","));
+			}
+			else
+			{
+				sql = String.Format(mainSql, "", "");
+			}
+
+			if (Full)
+				sql += additionalSql;
+
+			With.Connection(c => {
+				var adapter = new MySqlDataAdapter(sql, c);
+				adapter.SelectCommand.Parameters.AddWithValue("?StartDateParam", fromDate);
+				adapter.SelectCommand.Parameters.AddWithValue("?EndDateParam", toDate.AddDays(1));
+				adapter.SelectCommand.Parameters.AddWithValue("?RegionMaskParam", regionMask & SecurityContext.Administrator.RegionMask);
+				adapter.Fill(data);
+			});
+			return data;
+		}
+	}
+}
