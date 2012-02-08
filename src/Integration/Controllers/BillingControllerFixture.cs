e62d043a0e9c73b15b22611b1ae2cdf31a35f645
Золотарев Александр <a.zolotarev@analit.net>
2012-02-08 12:34:30 +0400

    Написал тесты для сообщений отключенный клиентов

--- src/Integration/Controllers/BillingControllerFixture.cs
+++ src/Integration/Controllers/BillingControllerFixture.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using AdminInterface.Controllers;
+using AdminInterface.Models;
 using AdminInterface.Models.Logs;
 using AdminInterface.Models.Suppliers;
 using Castle.ActiveRecord;
@@ -13,14 +14,27 @@ namespace Integration.Controllers
 	{
 		private BillingController controller;
 		private Supplier supplier;
+		private Client client;
 
 		[SetUp]
 		public void Setup()
 		{
 			supplier = DataMother.CreateSupplier();
 			supplier.Save();
+			client = DataMother.TestClient();
+			client.Save();
 			controller = new BillingController();
 			PrepareController(controller);
+		}
+
+		[Test]
+		public void Update_client_status()
+		{
+			controller.UpdateClientStatus(client.Id, false);
+			scope.Flush();
+
+			var logs = ClientInfoLogEntity.Queryable.Where(l => l.ObjectId == client.Id).ToList();
+			Assert.That(logs.FirstOrDefault(l => l.Message == "$$$Клиент отключен" && l.Type == LogObjectType.Client), Is.Not.Null, logs.Implode());
 		}
 
 		[Test]
@@ -35,7 +49,7 @@ namespace Integration.Controllers
 			var message = notifications.First();
 			Assert.That(message.Subject, Is.EqualTo("Приостановлена работа поставщика"), notifications.Implode(n => n.Subject));
 			var logs = ClientInfoLogEntity.Queryable.Where(l => l.ObjectId == supplier.Id).ToList();
-			Assert.That(logs.FirstOrDefault(l => l.Message == "$$$Клиент отключен"), Is.Not.Null, logs.Implode());
+			Assert.That(logs.FirstOrDefault(l => l.Message == "$$$Клиент отключен" && l.Type == LogObjectType.Supplier), Is.Not.Null, logs.Implode());
 		}
 	}
 }
