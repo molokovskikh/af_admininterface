2162d9d059db91dedbd51a0e42fe230c5b85d00f
Золотарев Александр <a.zolotarev@analit.net>
2012-02-24 16:12:30 +0400

    Дописал тест для требования #8439

---
+++ src/Functional/Billing/EditFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdminInterface.Models.Billing;
+using Functional.ForTesting;
+using Integration.ForTesting;
+using NUnit.Framework;
+using WatiN.Core;
+using WatiN.Core.Native.Windows;
+
+namespace Functional.Billing
+{
+	[TestFixture]
+	public class EditFixture : WatinFixture2
+	{
+		[Test]
+		public void Set_null_recipient()
+		{
+			var payer = DataMother.CreatePayer();
+			payer.Recipient = Recipient.FindFirst();
+			payer.Save();
+			using (var browser = Open(string.Format("Billing/Edit?BillingCode={0}#tab-mail", payer.Id))) {
+				var selectList = browser.SelectList(Find.ByName("Instance.Recipient.Id"));
+				var items = selectList.Options;
+				Console.WriteLine(items[0].Value);
+				selectList.SelectByValue(items[0].Value);
+				Click("Сохранить");
+			}
+			scope.Flush();
+			payer.Refresh();
+			Assert.IsNull(payer.Recipient);
+		}
+	}
+}
