6b143b72972a8386fca38395ebe22ca9d14e0f20
Золотарев Александр <a.zolotarev@analit.net>
2012-02-27 12:13:31 +0400

    удалил ;

--- src/Integration/ForTesting/DataMother.cs
+++ src/Integration/ForTesting/DataMother.cs
@@ -239,7 +239,7 @@ namespace Integration.ForTesting
 		{
 			var supplier = CreateSupplier();
 			var user = new User(supplier.Payer, supplier) {
-				Login = DateTime.Now.ToString();
+				Login = DateTime.Now.ToString()
 			};
 			user.Save();
 			user.Setup();
