d8b060ff599fbcb0d632b67aea2b45225dd496d5
Золотарев Александр <a.zolotarev@analit.net>
2012-05-11 19:19:54 +0400

    Написал базовый тест для просмотре статики по сертификатам

---
+++ src/Functional/LogsFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Test.Support.Web;
+using WatiN.Core;
+
+namespace Functional
+{
+	[TestFixture]
+	public class LogsFixture : WatinFixture2
+	{
+		[Test]
+		public void ShowTest()
+		{
+			Open("Main/Stat");
+			var link = browser.Elements.FirstOrDefault(e => e.Parent != null && e.Parent.Id == "StatisticsTD");
+			link.Click();
+			AssertText("Статистика по сертификатам");
+		}
+	}
+}
