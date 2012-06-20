e80baf00efbe38488d2134b13f3c5e88b500c896
Квасов Роман <r.kvasov@analit.net>
2012-06-20 13:17:15 +0400

    миграция для исправления баланса Ошибка #10419 Баланс б.к. 5739 (Пожидаева)

---
+++ migrations/537_00_fix_balance.sql
@@ -0,0 +1,11 @@
+update Billing.Payers
+set Balance = Balance + 4800
+where PayerId = 5739;
+
+update Billing.Payers
+set Balance = Balance + 2400
+where PayerId = 5031;
+
+update Billing.Payers
+set Balance = Balance + 800
+where PayerId = 3773;
