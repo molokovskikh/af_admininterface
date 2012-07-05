3acede25f44b7bed7e136fb6eb80ba717c2e14f0
Квасов Роман <r.kvasov@analit.net>
2012-07-05 11:57:26 +0400

    дописал тест

--- test/registration.coffee
+++ test/registration.coffee
@@ -4,3 +4,12 @@ $ ->
 		model.forSupplier(true)
 		equal model.isHiddenFromSupplier(), true
 		equal model.isHiddenFromSupplierEnable(), false
+
+	test "update orgs after payer id change", ->
+		model = new RegistrationViewModel()
+		ajaxData = [{id: 1, name: "1"}, {id: 2, name: "2"}]
+		$.extend
+			get: (url, data, callback) ->
+				callback(ajaxData)
+		model.payerId(1)
+		equal model.orgs().length, 2
--- test/registration.js
+++ test/registration.js
@@ -1,11 +1,31 @@
 (function() {
   $(function() {
-    return test("set is hidden for supplier if register copy for supplier", function() {
+    test("set is hidden for supplier if register copy for supplier", function() {
       var model;
       model = new RegistrationViewModel();
       model.forSupplier(true);
       equal(model.isHiddenFromSupplier(), true);
       return equal(model.isHiddenFromSupplierEnable(), false);
+    });
+    return test("update orgs after payer id change", function() {
+      var ajaxData, model;
+      model = new RegistrationViewModel();
+      ajaxData = [
+        {
+          id: 1,
+          name: "1"
+        }, {
+          id: 2,
+          name: "2"
+        }
+      ];
+      $.extend({
+        get: function(url, data, callback) {
+          return callback(ajaxData);
+        }
+      });
+      model.payerId(1);
+      return equal(model.orgs().length, 2);
     });
   });
 }).call(this);
