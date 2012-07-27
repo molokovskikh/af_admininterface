7093f9fab20b60f63ab2225d38cd7d3fad425236
Квасов Роман <r.kvasov@analit.net>
2012-07-27 18:09:05 +0400

    Ошибка #11067 Просьба удалить серую раскраску с логинов Поставщиков

---
+++ src/Unit/Models/UserSearchItemFixture.cs
@@ -0,0 +1,19 @@
+﻿using AdminInterface.Models;
+using AdminInterface.Models.Billing;
+using NUnit.Framework;
+
+namespace Unit.Models
+{
+	[TestFixture]
+	public class UserSearchItemFixture
+	{
+		[Test]
+		public void Supplier_user_can_not_be_old()
+		{
+			var item = new UserSearchItem {
+				ClientType = SearchClientType.Supplier
+			};
+			Assert.That(item.IsOldUserUpdate, Is.False);
+		}
+	}
+}
