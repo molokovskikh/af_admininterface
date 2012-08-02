41eaaae62848cfc465ec9b681fb84c72da4d50ff
Золотарев Александр <a.zolotarev@analit.net>
2012-08-02 11:29:13 +0400

    Вернул класс для запуска методов PriceProcessor в исходное состояние

--- src/AdminInterface/Helpers/RemoteServiceHelper.cs
+++ src/AdminInterface/Helpers/RemoteServiceHelper.cs
@@ -23,7 +23,7 @@ namespace AdminInterface.Helpers
 	{
 		private static readonly ILog _log = LogManager.GetLogger(typeof(RemoteServiceHelper));
 
-		public static TResult RemotingCall<TInterface, TResult>(Func<TInterface, TResult> action)
+		public static void RemotingCall(Action<IRemotePriceProcessor> action)
 		{
 			var binding = new NetTcpBinding();
 			binding.Security.Transport.ProtectionLevel = ProtectionLevel.EncryptAndSign;
@@ -32,24 +32,17 @@ namespace AdminInterface.Helpers
 			binding.MaxReceivedMessageSize = Int32.MaxValue;
 			binding.MaxBufferSize = 524288;
 
-			var wcfUrl = string.Empty;
-			if (typeof(TInterface) == typeof(IRemotePriceProcessor))
-				wcfUrl = Settings.Default.WCFServiceUrl;
-			if (typeof(TInterface) == typeof(IRemoteOrderSenderService))
-				wcfUrl = Settings.Default.WCFOrderSenderServiceUrl;
-
-			var channelFactory = new ChannelFactory<TInterface>(binding, wcfUrl);
-			TInterface channel = default(TInterface);
-			TResult result = default (TResult);
+			var channelFactory = new ChannelFactory<IRemotePriceProcessor>(binding, Settings.Default.WCFServiceUrl);
+			IRemotePriceProcessor channel = null;
 			try
 			{
 				channel = channelFactory.CreateChannel();
-				result = action(channel);
+				action(channel);
 				((ICommunicationObject)channel).Close();
 			}
 			catch (Exception e)
 			{
-				_log.Warn("Ошибка при обращении к сервису", e);
+				_log.Warn("Ошибка при обращении к сервису обработки прайс листов", e);
 			}
 			finally
 			{
@@ -59,7 +52,6 @@ namespace AdminInterface.Helpers
 					communicationObject.Abort();
 				channelFactory.Close();
 			}
-			return result;
 		}
 
 		public static ServiceStatus GetServiceStatus(string host, string serviceName)
