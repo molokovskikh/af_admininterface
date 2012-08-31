1df0692cf22a72dfd90c1f9b3325955807261af5
Александр Шилин <a.shilin@analit.net>
2012-08-31 10:11:29 +0400

    Ошибка #11557. Назначение форматера на странице настройки поставщика. Выполнена генерация Специального формата при выборе нового форматера

---
+++ src/Integration/ManagepFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using AddUser;
+using AdminInterface.Models;
+using AdminInterface.Models.Suppliers;
+using Castle.ActiveRecord;
+using Castle.ActiveRecord.Framework.Config;
+using NHibernate.Linq;
+using NUnit.Framework;
+using Test.Support;
+using Test.Support.Suppliers;
+
+
+namespace Integration
+{
+	public class ManagepFixture : Test.Support.IntegrationFixture
+	{
+		private TestSupplier _supplier;
+		[SetUp]
+		public void SetUp()
+		{
+			_supplier = TestSupplier.Create();
+		}
+		[Test]
+		public void NewSpecialOrderHandlerTest()
+		{
+			var handler = session.Query<TestHandler>().First(t => t.ClassName.Contains("Formater") && !t.ClassName.Contains("Default"));
+			var supplier = session.Query<Supplier>().First(t => t.Id == _supplier.Id);
+
+			managep.CreateNewSpecialOrders(supplier, handler.Id);
+			var specHandler = session.Query<SpecialHandler>().First(t => t.Supplier == supplier && t.Handler.Id == handler.Id);
+			Assert.That(specHandler.Name, Is.EqualTo("Специальный формат"));
+
+			var specCount = session.Query<SpecialHandler>().Count();
+			var defHandler = session.Query<OrderHandler>().First(t => t.ClassName == "DefaultXmlFormater");
+			managep.CreateNewSpecialOrders(supplier, defHandler.Id);
+			Assert.That(specCount, Is.EqualTo(session.Query<SpecialHandler>().Count()));
+		}
+	}
+}
