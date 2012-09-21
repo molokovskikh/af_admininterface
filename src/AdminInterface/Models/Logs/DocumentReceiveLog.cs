01e8395940cdc66f5e1f4bb35e9d010115f2d71f
Александр Шилин <a.shilin@analit.net>
2012-09-21 11:26:32 +0400

    Новшество #11701. Мониторинг неработающих клиентов/адресов, кому направляются накладные отказы. Чиню тесты.

--- src/AdminInterface/Models/Logs/DocumentReceiveLog.cs
+++ src/AdminInterface/Models/Logs/DocumentReceiveLog.cs
@@ -18,14 +18,6 @@ namespace AdminInterface.Models.Logs
 		[Description("Документы от АК \"Инфорум\"")] InforoomDoc = 3
 	}
 
-	public enum CheckType
-	{
-		Ready = 1,
-		[Description("Адрес отключен")] AddressDisable = 2,
-		[Description("Адрес недоступен поставщику")]AddressNotAssign = 3,
-		[Description("Пользователь не обновлялся более месяца")]UserNotUpdate = 4
-	}
-
 	[ActiveRecord("DocumentSendLogs", Schema = "Logs", Lazy = true)]
 	public class DocumentSendLog
 	{
@@ -114,9 +106,6 @@ namespace AdminInterface.Models.Logs
 		[HasMany(Inverse = true, Lazy = true)]
 		public virtual IList<DocumentSendLog> SendLogs { get; set; }
 
-		[BelongsTo("FirmCode")]
-		public virtual Supplier Supplier { get; set; }
-
 		public virtual string GetRemoteFileName(AppConfig config)
 		{
 			if (String.IsNullOrEmpty(FileName))
@@ -128,35 +117,6 @@ namespace AdminInterface.Models.Logs
 
 			var file = String.Format("{0}_{1}({2}){3}", Id, FromSupplier.Name, Path.GetFileNameWithoutExtension(FileName), Path.GetExtension(FileName));
 			return Path.Combine(config.AptBox, Address.Id.ToString(), DocumentType.ToString() + "s", file);
-		}
-
-		public virtual String CheckReason
-		{
-			get
-			{
-				var result = new List<string>();
-				if (!Address.Enabled || !Address.Client.Enabled)
-					result.Add(CheckType.AddressDisable.GetDescription());
-
-				if ((Address.Client.MaskRegion & Supplier.RegionMask) == 0)
-					result.Add(CheckType.AddressNotAssign.GetDescription());
-				var holder = ActiveRecordMediator.GetSessionFactoryHolder();
-				var session = holder.CreateSession(typeof(ActiveRecordBase));
-				try {
-					var lastUpdate = session.CreateSQLQuery(@"select max(AFTime)
-from logs.AuthorizationDates d
-join Customers.UserAddresses ua on ua.UserId = d.UserId
-where ua.AddressId = :addressId")
-						.SetParameter("addressId", Address.Id)
-						.UniqueResult<DateTime?>();
-					if (lastUpdate == null || lastUpdate.Value < DateTime.Now.AddMonths(-1))
-						result.Add(CheckType.UserNotUpdate.GetDescription());
-				}
-				finally {
-					holder.ReleaseSession(session);
-				}
-				return String.Join(", ", result);
-			}
 		}
 	}
 }
