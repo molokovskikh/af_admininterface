c6da641e929ce827429425710de75e3b8b1baa5b
Александр Шилин <a.shilin@analit.net>
2012-09-27 10:56:56 +0400

    Новшество i11701. Мониторинг неработающих клиентов/адресов, кому направляются накладные отказы. Исправлена страница отчета - добавлен счетчик накладных и фильтр по времени. Добавлена миграция для создания таблицы для отклоненных накладных

---
+++ src/AdminInterface/Models/Logs/RejectWaybillLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AdminInterface.Models.Suppliers;
+using Castle.ActiveRecord;
+
+namespace AdminInterface.Models.Logs
+{
+	[ActiveRecord(Schema = "Logs", Lazy = true)]
+	public class RejectWaybillLog
+	{
+		[PrimaryKey("RowId")]
+		public virtual uint Id { get; set; }
+
+		[BelongsTo("FirmCode")]
+		public virtual Supplier FromSupplier { get; set; }
+
+		[BelongsTo("ClientCode")]
+		public virtual Client ForClient { get; set; }
+
+		[BelongsTo("AddressId")]
+		public virtual Address Address { get; set; }
+
+		[Property]
+		public virtual DateTime LogTime { get; set; }
+
+		[Property]
+		public virtual string FileName { get; set; }
+
+		[Property]
+		public virtual string Addition { get; set; }
+
+		[Property]
+		public virtual ulong DocumentSize { get; set; }
+	}
+}
