df887b07f27bea9a0a63dd1bf3be900451fcaca7
Александр Шилин <a.shilin@analit.net>
2012-10-04 09:35:02 +0400

    Новшество #11701. Мониторинг неработающих клиентов/адресов, кому направляются накладные отказы. Исправлен фильтр, добавлены колонки с поставщиком

--- src/AdminInterface/Controllers/ManagerReportsController.cs
+++ src/AdminInterface/Controllers/ManagerReportsController.cs
@@ -36,13 +36,12 @@ namespace AdminInterface.Controllers
 	public class RejectCounts
 	{
 		public uint ClientId { get; set; }
+		public uint SupplierId { get; set; }
 		public uint AddressId { get; set; }
 		public int Count { get; set; }
 		public string ClientName { get; set; }
+		public string SupplierName { get; set; }
 		public string AddressName { get; set; }
-		public ClientStatus ClientStatus { get; set; }
-		public bool AddressEnabled { get; set; }
-		public bool IsUpdate { get; set; }
 		public string RegionName { get; set; }
 		public RejectReasonType? RejectReason { get; set; }
 		public string RejectReasonName
@@ -66,7 +65,9 @@ namespace AdminInterface.Controllers
 				{ "AddressId", "AddressId" },
 				{ "AddressName", "AddressName" },
 				{ "ClientId", "ClientId" },
-				{ "ClientName", "ClientName" }
+				{ "ClientName", "ClientName" },
+				{ "SupplierId", "SupplierId" },
+				{ "SupplierName", "SupplierName" }
 			};
 			PageSize = 30;
 			Period = new DatePeriod(DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1));
@@ -97,57 +98,27 @@ namespace AdminInterface.Controllers
 			criteria.CreateCriteria("ForClient", "c", JoinType.LeftOuterJoin)
 				.Add(Expression.Sql("{alias}.RegionCode & " + regionMask + " > 0"));
 			criteria.CreateAlias("Address", "a", JoinType.LeftOuterJoin);
+			criteria.CreateAlias("FromSupplier", "f", JoinType.LeftOuterJoin);
 
 			criteria.SetProjection(Projections.ProjectionList()
 				.Add(Projections.Count("Id").As("Count"))
 				.Add(Projections.GroupProperty("c.Id").As("ClientId"))
 				.Add(Projections.GroupProperty("a.Id").As("AddressId"))
+				.Add(Projections.GroupProperty("f.Id").As("SupplierId"))
 				.Add(Projections.GroupProperty("RejectReason").As("RejectReason"))
 				.Add(Projections.Property("c.Name").As("ClientName"))
 				.Add(Projections.Property("a.Value").As("AddressName"))
-				.Add(Projections.Property("c.Status").As("ClientStatus"))
-				.Add(Projections.Property("a.Enabled").As("AddressEnabled"))
-				.Add(Projections.Property("c.HomeRegion").As("RegionName")));
+				.Add(Projections.Property("c.HomeRegion").As("RegionName"))
+				.Add(Projections.Property("f.Name").As("SupplierName")));
 			criteria.Add(Expression.Ge("LogTime", Period.Begin.Date))
 				.Add(Expression.Le("LogTime", Period.End.Date));
 			return criteria;
-		}
-
-		public List<Address> Sort(List<Address> list)
-		{
-			var sortProperty = GetSortProperty();
-			switch (sortProperty) {
-				case "Id":
-					if(SortDirection == "asc")
-						return list.OrderBy(t => t.Id).ToList();
-					return list.OrderByDescending(t => t.Id).ToList();
-				case "c.Id":
-					if(SortDirection == "asc")
-						return list.OrderBy(t => t.Client.Id).ToList();
-					return list.OrderByDescending(t => t.Client.Id).ToList();
-				case "Name":
-					if(SortDirection == "asc")
-						return list.OrderBy(t => t.Value).ToList();
-					return list.OrderByDescending(t => t.Value).ToList();
-				case "c.Name":
-					if(SortDirection == "asc")
-						return list.OrderBy(t => t.Client.Name).ToList();
-					return list.OrderByDescending(t => t.Client.Name).ToList();
-			}
-			return new List<Address>();
 		}
 
 		public IList<RejectCounts> Find()
 		{
 			var criteria = GetCriteria();
 			var result = AcceptPaginator(criteria);
-			ArHelper.WithSession(s => {
-				foreach (var row in result) {
-					var address = s.Query<Address>().FirstOrDefault(a => a.Id == row.AddressId);
-					if(address != null)
-						row.IsUpdate = address.AvaliableForUsers.Count(u => u.Logs.AFTime >= DateTime.Now.AddMonths(-1)) != 0;
-				}
-			});
 			return result;
 		}
 	}
