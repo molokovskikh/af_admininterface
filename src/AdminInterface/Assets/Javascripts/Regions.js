5eddfe389edabcce086933f87f3817806dc7247d
Александр Шилин <a.shilin@analit.net>
2012-10-30 12:39:49 +0400

    Новшество #12209. Страница 'Редактирование регионов' - реорганизацияСтраница 'Редактирование регионов' - реорганизация. Добавил в редактирование региона поля временного сдвига от Москвы и телефона по умолчанию, в таблицу списка регионов добавил дополнительные колонки из таблицы регионов

---
+++ src/AdminInterface/Assets/Javascripts/Regions.js
@@ -0,0 +1,10 @@
+﻿function ShowDefaultRegions() {
+	if (jQuery("#ShowRegionsLink")[0].innerHTML == "Скрыть") {
+		$('#EditDefaultRegions').hide();
+		jQuery("#ShowRegionsLink")[0].innerHTML = "Редактировать регионы работы по умолчанию";
+	}
+	else {
+		$('#EditDefaultRegions').show();
+		jQuery("#ShowRegionsLink")[0].innerHTML = "Скрыть";
+	}
+}
