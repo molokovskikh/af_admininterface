9a70f7420fa4a91c499189524b732e21c375f487
Квасов Роман <r.kvasov@analit.net>
2012-11-01 14:04:23 +0400

    Ошибка #12795 Невозможно зарегистрировать адрес в мультиюриковом клиенте.

--- src/AdminInterface/MonoRailExtentions/RecreateOnlyIfNullBinder.cs
+++ src/AdminInterface/MonoRailExtentions/RecreateOnlyIfNullBinder.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using Castle.ActiveRecord.Framework.Internal;
 using Castle.MonoRail.ActiveRecordSupport;
 using Castle.MonoRail.Framework;
 
@@ -6,9 +8,30 @@ namespace AdminInterface.MonoRailExtentions
 {
 	public class RecreateOnlyIfNullBinder : ARDataBinder
 	{
+		private static MethodInfo obtainPrimaryKeyValue;
+		private static MethodInfo isValidKey;
+
+		static RecreateOnlyIfNullBinder()
+		{
+			obtainPrimaryKeyValue = typeof(ARDataBinder).GetMethod("ObtainPrimaryKeyValue", BindingFlags.NonPublic | BindingFlags.Instance);
+			isValidKey = typeof(ARDataBinder).GetMethod("IsValidKey", BindingFlags.NonPublic | BindingFlags.Instance);
+		}
+
 		protected override bool ShouldRecreateInstance(object value, System.Type type, string prefix, Castle.Components.Binder.Node node)
 		{
-			return value == null;
+			if (value == null)
+				return true;
+
+			//нужно пересоздавать только объекты у которыех есть id те те объекты которые нужно загрузить из базы
+			if (node != null && CurrentARModel != null) {
+				if (IsBelongsToRef(CurrentARModel, prefix)) {
+					var model = ActiveRecordModel.GetModel(type);
+					PrimaryKeyModel pkModel = null;
+					var id = obtainPrimaryKeyValue.Invoke(this, new object[] { model, node, prefix, pkModel });
+					return (bool)isValidKey.Invoke(this, new[] { id });
+				}
+			}
+			return false;
 		}
 
 		public static void Prepare(SmartDispatcherController controller, string expect = null)
