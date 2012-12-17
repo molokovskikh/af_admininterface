5b56a30605e980e491445bd860cf6539b74406cb
Золотарев Александр <a.zolotarev@analit.net>
2012-12-17 21:37:44 +0400

    Выполнил #13251 Сбросить версию АФ

---
+++ src/AdminInterface/Assets/Javascripts/UserScripts.js
@@ -0,0 +1,8 @@
+﻿function ResetAFVersion(userId, userName) {
+	var dialogText = "Вы уверены, что хотите произвести  сброс параметра, отвечающего за версию АФ, ниже кот. пользователю не разрешаем обновляться до 999? (Пользователь: " + userName + ")";
+	YesNoDialog("Сбросить версию АФ", dialogText, function () {
+		$.post("ResetAFVersion", { userId: userId }, function (data) {
+			window.location = "Settings";
+		});
+	});
+}
