90859502a6f254c795d421de5abfe8a808e8371a
Александр Шилин <a.shilin@analit.net>
2013-01-15 13:13:16 +0400

    Новшество #12163. Автоматизация публикации режима работы ТП в разных регионах. Добавлено редактирование общего шаблона и времени по Москве для режима работы техподдержки.

---
+++ migrations/745_00_AddTechOperatingDataToDefaults.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE `usersettings`.`defaults` MODIFY COLUMN `AnalitFVersion` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT 0,
+ ADD COLUMN `TechOperatingModeTemplate` TEXT AFTER `AddressesHelpText`,
+ ADD COLUMN `TechOperatingModeBegin` VARCHAR(5) NOT NULL DEFAULT '' AFTER `TechOperatingModeTemplate`,
+ ADD COLUMN `TechOperatingModeEnd` VARCHAR(5) NOT NULL DEFAULT '' AFTER `TechOperatingModeBegin`;
+
+ update `usersettings`.`defaults` set TechOperatingModeTemplate = '<p>будни: с 7.00 до 19.00</p>',
+TechOperatingModeBegin = '7.00',
+TechOperatingModeEnd = '19.00';
