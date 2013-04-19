fd0afde91ada1719a065d2914b6c31ac1a579ce6
Золотарев Александр <a.zolotarev@analit.net>
2013-04-19 12:00:35 +0400

    Починил страницу регистрации плательщика, вернул валидацию на страницу

---
+++ src/AdminInterface/Assets/Javascripts/RegisterPayer.js
@@ -0,0 +1,58 @@
+﻿$(function () {
+
+	if ($('#PaymentOptions_WorkForFree') != null) {
+		$('#PaymentOptions_WorkForFree').change(function () {
+
+			if (this.checked) {
+				$('#PaymentOptions_PaymentPeriodBeginDate').attr('disabled', 'disabled');
+				$('#PaymentOptions_Comment').attr('disabled', 'disabled');
+			}
+			else {
+				$('#PaymentOptions_PaymentPeriodBeginDate').removeAttr('disabled');
+				$('#PaymentOptions_Comment').removeAttr('disabled');
+			}
+		});
+	}
+
+	$.validator.addMethod("CommentValidator", function (value, element, params) {
+		var checkBox = $('PaymentOptions_WorkForFree');
+		if (checkBox != null && $('PaymentOptions_WorkForFree').checked)
+			return true;
+
+		if (!$("PaymentOptions_Comment").empty())
+			return true;
+
+		var maxFreeWorkWithoutMessage = Date.today().add(45).days();
+		var paymentPeriodBeginDate = Date.parse($('#PaymentOptions_PaymentPeriodBeginDate').val());
+		return paymentPeriodBeginDate < maxFreeWorkWithoutMessage;
+	}, "Клиент слишком долго работает бесплатно, нужно указать причину.");
+
+	$.validator.addMethod("DateInFuture", function (value, element, params) {
+		var checkBox = $('PaymentOptions_WorkForFree');
+		if (checkBox != null && $('PaymentOptions_WorkForFree').checked)
+			return true;
+
+		var paymentPeriodBeginDate = Date.parse($('#PaymentOptions_PaymentPeriodBeginDate').val());
+		return paymentPeriodBeginDate >= Date.today();
+	}, "Дата начала платного периода меньше текущей.");
+
+	$('#PayerInfo').validate();
+
+	$('#PaymentOptions_PaymentPeriodBeginDate').rules("add", {
+		date: true, DateInFuture: true,
+		messages: {
+			date: "Ошибка ввода даты"
+		}
+	});
+
+	$('#Instance_Name').rules("add", {
+		required: true,
+		messages: {
+			required: "Введите краткое наименование"
+		}
+	});
+
+	$('#PaymentOptions_Comment').rules("add", {
+		CommentValidator: true
+	});
+});
