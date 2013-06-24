754a5a19024f2b20159f9d60bb67f7d16f7aea65
Квасов Роман <r.kvasov@analit.net>
2013-06-24 11:39:17 +0400

    Ошибка #16023 Неоднозначность в визуальном логировании действий по поставщику

--- src/Integration/Audit/MaskedAuditablePropertyFixture.cs
+++ src/Integration/Audit/MaskedAuditablePropertyFixture.cs
@@ -34,5 +34,12 @@ namespace Integration
 			var property = new MaskedAuditableProperty(typeof(Test).GetProperty("MaskRegion"), "Регион", 0UL, 18446742976345407488UL);
 			Assert.That(property.ToString(), Is.StringContaining("$$$Изменено 'Регион' Удалено 'Ижевск'"));
 		}
+
+		[Test]
+		public void Disable_all_regions()
+		{
+			var property = new MaskedAuditableProperty(typeof(Test).GetProperty("MaskRegion"), "Регион", 0UL, ulong.MaxValue);
+			Assert.That(property.ToString(), Is.EqualTo("$$$Изменено 'Регион' Удалено 'Все регионы'"));
+		}
 	}
 }
