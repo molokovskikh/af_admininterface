0bd321046d1f324f22fb5cce3175a7d19feef7da
Квасов Роман <r.kvasov@analit.net>
2013-07-05 17:25:20 +0400

    исправил тест

--- src/Functional/Billing/PayerSecurityFixture.cs
+++ src/Functional/Billing/PayerSecurityFixture.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using AdminInterface.Models.Security;
+using Common.Tools;
 using Integration.ForTesting;
+using NHibernate.Linq;
 using NUnit.Framework;
 using Test.Support.Web;
 using WatiN.Core;
@@ -10,16 +14,32 @@ namespace Functional.Billing
 	[TestFixture]
 	public class PayerSecurityFixture : WatinFixture2
 	{
+		private List<Administrator> administrators;
+		private Permission permission;
+
+		[SetUp]
+		public void Setup()
+		{
+			administrators = session.Query<Administrator>()
+				.Where(a => a.UserName == Environment.UserName)
+				.ToList();
+			permission = session.Query<Permission>()
+				.First(p => p.Type == PermissionType.ChangePayment);
+			administrators.ForEach(a => a.AllowedPermissions.Remove(permission));
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			administrators.Each(a => a.AllowedPermissions.Add(permission));
+		}
+
 		[Test]
 		public void Do_not_edit_payment_without_permission()
 		{
 			var client = DataMother.CreateTestClientWithUser();
 			var user = client.Users[0];
 			var payer = user.Payer;
-
-			var admin = Administrator.GetByName(Environment.UserName);
-			admin.RemovePermission();
-			Save(admin);
 			session.Flush();
 
 			Open(payer);
