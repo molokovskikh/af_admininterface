e32f0e0ce151850fa3d323633b8ac96e6d20bb81
Квасов Роман <r.kvasov@analit.net>
2013-12-25 12:34:50 +0400

    Исправил сборку

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var name = /([^/]+)\.html/.exec(window.document.URL)[1]
 configPaths = {
-	"jquery": "packages/jQuery.1.6.2/Content/Scripts/jquery-1.6.2",
+	"jquery": "packages/jQuery.1.6.4/Content/Scripts/jquery-1.6.4",
 	"jquery-ui": "packages/jQuery.UI.Combined.1.10.2/Content/Scripts/jquery-ui-1.10.2",
 	"jquery-validate": "packages/jQuery.Validation.1.8.1/Content/Scripts/jquery.validate",
 	"knockout": "packages/knockoutjs.2.1.0/Content/Scripts/knockout-2.1.0.debug",
