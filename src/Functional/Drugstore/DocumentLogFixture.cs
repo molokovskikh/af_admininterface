9de0194e065172ee9330b521fedc02a6c82b8d71
Квасов Роман <r.kvasov@analit.net>
2016-08-16 17:29:31 +0300

    Исправил тесты

--- src/Functional/Drugstore/DocumentLogFixture.cs
+++ src/Functional/Drugstore/DocumentLogFixture.cs
@@ -2,24 +2,20 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using AdminInterface.Models;
 using AdminInterface.Models.Logs;
 using AdminInterface.Models.Suppliers;
-using Castle.ActiveRecord;
 using Common.Web.Ui.Models;
+using Common.Web.Ui.NHibernateExtentions;
 using Functional.ForTesting;
-using Integration.ForTesting;
 using NUnit.Framework;
 using Test.Support;
-using Test.Support.Web;
 using WatiN.Core;
-using WatiN.Core.Native.Windows;
 
 namespace Functional.Drugstore
 {
 	[TestFixture]
-	public class DocumentLogFixture : FunctionalFixture
+	public class DocumentLogFixture : AdmSeleniumFixture
 	{
 		private Client client;
 		private Supplier supplier;
@@ -37,7 +33,7 @@ namespace Functional.Drugstore
 			supplier = DataMother.CreateSupplier();
 
 			session.SaveOrUpdate(client);
-			Save(supplier);
+			session.Save(supplier);
 
 			document = new DocumentReceiveLog(supplier) {
 				FileName = "test.txt",
@@ -48,6 +44,11 @@ namespace Functional.Drugstore
 			sendLog = new DocumentSendLog(user, document);
 
 			Save(document, sendLog);
+		}
+
+		public void Save(params object[] e)
+		{
+			session.SaveMany(e);
 		}
 
 		[Test]
@@ -61,11 +62,18 @@ namespace Functional.Drugstore
 			AssertText("тестовый адрес");
 		}
 
+		private void OpenedWindow(string text)
+		{
+			Assert.That(browser.WindowHandles.Count, Is.GreaterThan(1));
+			browser.SwitchTo().Window(browser.WindowHandles.Last());
+			WaitForText(text);
+		}
+
 		[Test]
 		public void Resend_document()
 		{
 			Open("Logs/Documents?filter.Client.Id={0}", client.Id);
-			browser.CheckBox(Find.ByName("ids[0]")).Click();
+			browser.FindElementByName("ids[0]").Click();
 			Click("Повторить");
 			AssertText("Документы будут отправлены повторно");
 
@@ -77,15 +85,15 @@ namespace Functional.Drugstore
 		public void ShowDocumentForMultiUserClient()
 		{
 			var newUser = client.AddUser(session, "Новый тестовый пользователь");
-			Save(newUser);
+			session.Save(newUser);
 
 			var newSendLog = new DocumentSendLog(newUser, document);
-			Save(newSendLog);
+			session.Save(newSendLog);
 			Open("Logs/Documents?filter.Client.Id={0}", client.Id);
 			AssertText("Статистика");
-			Assert.That(browser.Text, Is.Not.StringContaining("Пользователь получивший документ"));
+			AssertNoText("Пользователь получивший документ");
 			Click("...");
-			browser.WaitUntilContainsText("Пользователь получивший документ", 1);
+			WaitForText("Пользователь получивший документ");
 			AssertText("Пользователь получивший документ");
 			AssertText(user.Login);
 		}
@@ -117,9 +125,10 @@ namespace Functional.Drugstore
 			Open("Logs/Documents?filter.Client.Id={0}", client.Id);
 
 			Click("SpecialFileNameForThisReport");
+			WaitAjax();
 			Click("123");
-			browser.WaitUntilContainsText("Сопоставлен с", 1);
-			AssertText("Сопоставлен с \"testCatName testForm\"");
+			WaitForText("Сопоставлен с");
+			AssertText("Сопоставлен с \"testCatalog\"");
 			AssertText("По производителю с \"testProducer\"");
 		}
 
@@ -132,12 +141,12 @@ namespace Functional.Drugstore
 			OpenedWindow("История документов");
 			//Смотрим, есть ли надпись "Клиенту"
 			AssertText("Клиенту");
-			//Смотрим, есть ли ссылка на поставщика
-			Assert.That(browser.Links.Select(l => l.Text == supplier.Name && l.Text.Contains("Suppliers//" + supplier.Id.ToString())).Count(), Is.GreaterThan(0));
-			//Смотрим, есть ли ссылка на клиента
-			Assert.That(browser.Links.Select(l => l.Text == client.Name && l.Text.Contains("Clients//" + client.Id.ToString())).Count(), Is.GreaterThan(0));
-			//Смотрим, есть ли ссылка на пользователя, получившего документ
-			Assert.That(browser.Links.Select(l => l.Text == user.Name && l.Text.Contains("Users//" + user.Id.ToString())).Count(), Is.GreaterThan(0));
+			////Смотрим, есть ли ссылка на поставщика
+			Assert.That(browser.FindElementsByCssSelector("a").Select(l => l.Text == supplier.Name && l.Text.Contains("Suppliers//" + supplier.Id.ToString())).Count(), Is.GreaterThan(0));
+			////Смотрим, есть ли ссылка на клиента
+			Assert.That(browser.FindElementsByCssSelector("a").Select(l => l.Text == client.Name && l.Text.Contains("Clients//" + client.Id.ToString())).Count(), Is.GreaterThan(0));
+			////Смотрим, есть ли ссылка на пользователя, получившего документ
+			Assert.That(browser.FindElementsByCssSelector("a").Select(l => l.Text == user.Name && l.Text.Contains("Users//" + user.Id.ToString())).Count(), Is.GreaterThan(0));
 		}
 
 		[Test]
@@ -158,10 +167,10 @@ namespace Functional.Drugstore
 			Open("Logs/Documents?filter.Client.Id={0}", client.Id);
 			AssertText("Тестовый поставщик");
 			Click("TestFile.txt");
-			browser.WaitUntilContainsText("Страна", 1);
+			WaitForText("Страна");
 			AssertText("Страна");
 			Click(line.Product);
-			browser.WaitUntilContainsText(filename, 1);
+			WaitForText(filename);
 			AssertText(filename);
 		}
 	}
