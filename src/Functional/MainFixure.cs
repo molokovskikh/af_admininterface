0ae23fbdad054eaab2a1ca8693543a154972cae0
Зылевич Вадим <01vadim10@gmail.net>
2016-09-02 17:30:05 +0300

    Перенес тест из Functional в Unit

--- src/Functional/MainFixure.cs
+++ src/Functional/MainFixure.cs
@@ -180,14 +180,5 @@ namespace Functional
 			var tokens = session.Query<FederalSupplierToken>().ToArray();
 			Assert.AreEqual(newToken, tokens.Implode(x => x.Name));
 		}
-
-		[Test]
-		public void CheckFormalizeTimeTest()
-		{
-			var nowDate = DateTime.Now.Date;
-			Assert.False(MainController.CheckFormalizeTime(nowDate.AddHours(4), nowDate.AddHours(4).AddMinutes(15).AddSeconds(1)));
-			Assert.False(MainController.CheckFormalizeTime(nowDate.AddHours(6), nowDate.AddHours(6).AddMinutes(15)));
-			Assert.True(MainController.CheckFormalizeTime(nowDate.AddHours(6), nowDate.AddHours(6).AddMinutes(15).AddSeconds(1)));
-		}
 	}
 }
